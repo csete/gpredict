6892d5d7d6fafed6ac7583206ad9260ced818bc2
csete <csete@ab6734ba-2843-0410-b83b-99c217ef6c0f>
2009-05-24 09:20:09 +0000

    Use field label instead of field hint as checkbox label. Use field hint as tooltip text.
    git-svn-id: https://gpredict.svn.sourceforge.net/svnroot/gpredict/trunk@326 ab6734ba-2843-0410-b83b-99c217ef6c0f

--- src/sat-pref-single-sat.c
+++ src/sat-pref-single-sat.c
@@ -50,6 +50,7 @@ static guint flags;
 static gboolean dirty = FALSE;
 static gboolean reset = FALSE;
 
+extern const gchar *SINGLE_SAT_FIELD_TITLE[];
 extern const gchar *SINGLE_SAT_FIELD_HINT[];
 
 static void toggle_cb  (GtkToggleButton *toggle, gpointer data);
@@ -100,7 +101,8 @@ GtkWidget *sat_pref_single_sat_create (GKeyFile *cfg)
 
 	for (i = 0; i < SINGLE_SAT_FIELD_NUMBER; i++) {
 
-		check[i] = gtk_check_button_new_with_label (SINGLE_SAT_FIELD_HINT[i]);
+        check[i] = gtk_check_button_new_with_label (SINGLE_SAT_FIELD_TITLE[i]);
+        gtk_widget_set_tooltip_text (check[i], SINGLE_SAT_FIELD_HINT[i]);
 
 		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (check[i]),
 					      flags & (1 << i));
