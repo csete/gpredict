0b8cadda4dd3fe9064db82406294a62fff5bd3dd
csete <csete@ab6734ba-2843-0410-b83b-99c217ef6c0f>
2009-08-04 10:29:58 +0000

    Added new satellite data files.
    git-svn-id: https://gpredict.svn.sourceforge.net/svnroot/gpredict/trunk@344 ab6734ba-2843-0410-b83b-99c217ef6c0f

---
+++ data/satdata/convert.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import os
+import string
+
+
+# For each input file
+for file in os.listdir("./in/"):
+
+    print file
+
+    # open TLE file for reading
+    tlefile = open('./in/'+file, 'r')
+
+    # create category file
+    category = "./out/"+file.partition(".")[0]+".cat"
+    catfile = open(category, 'w')
+
+    while 1:
+        # read three lines at a time; strip trailing whitespaces
+        line1 = tlefile.readline().strip()
+        if not line1: break
+        line2 = tlefile.readline().strip()
+        line3 = tlefile.readline().strip()
+
+        # catalog number
+        catnum = line2[2:7]
+        print " ... "+catnum
+
+        # add satellite to category
+        catfile.write(catnum+'\n')
+
+        # satellite file
+        satfile = open('./out/'+catnum+'.sat','w')
+
+        satfile.write('VERSION=1.1\n')
+        satfile.write('NAME='+line1+'\n')
+        satfile.write('NICKNAME='+line1+'\n')
+        satfile.write('TLE1='+line2+'\n')
+        satfile.write('TLE2='+line3+'\n')
+
+        satfile.close()
+
+    # close TLE and CAT files
+    tlefile.close()
+    catfile.close()
+
