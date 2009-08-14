d56d52d707914199510c4902cd7aedcc59a57a8a
csete <csete@ab6734ba-2843-0410-b83b-99c217ef6c0f>
2009-08-14 10:27:35 +0000

    Fixed indent.
    git-svn-id: https://gpredict.svn.sourceforge.net/svnroot/gpredict/trunk@416 ab6734ba-2843-0410-b83b-99c217ef6c0f

--- src/gtk-sat-module.h
+++ src/gtk-sat-module.h
@@ -11,17 +11,17 @@
     More details can be found at the project home page:
 
             http://gpredict.oz9aec.net/
- 
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
-  
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-  
+
     You should have received a copy of the GNU General Public License
     along with this program; if not, visit http://www.fsf.org/
 */
@@ -42,40 +42,40 @@ extern "C" {
 
 /** \brief The state of a module */
 typedef enum {
-	GTK_SAT_MOD_STATE_DOCKED = 0,   /*!< The module is docked into the notebook. */
-	GTK_SAT_MOD_STATE_WINDOW,       /*!< The module is in it's own window. */
-	GTK_SAT_MOD_STATE_FULLSCREEN    /*!< The module is in FULLSCREEN mode :-) */
+    GTK_SAT_MOD_STATE_DOCKED = 0,   /*!< The module is docked into the notebook. */
+    GTK_SAT_MOD_STATE_WINDOW,       /*!< The module is in it's own window. */
+    GTK_SAT_MOD_STATE_FULLSCREEN    /*!< The module is in FULLSCREEN mode :-) */
 } gtk_sat_mod_state_t;
 
 
 /** \brief Module layout */
 typedef enum {
-	GTK_SAT_MOD_LAYOUT_1 = 0, /*!< one view */
-	GTK_SAT_MOD_LAYOUT_2,     /*!< Two views, one above the other */
-	GTK_SAT_MOD_LAYOUT_3,     /*!< Three views, big one on top, two small one at bottom */
-	GTK_SAT_MOD_LAYOUT_4      /*!< Three views, big one on bottom, small ones on top */  
+    GTK_SAT_MOD_LAYOUT_1 = 0, /*!< one view */
+    GTK_SAT_MOD_LAYOUT_2,     /*!< Two views, one above the other */
+    GTK_SAT_MOD_LAYOUT_3,     /*!< Three views, big one on top, two small one at bottom */
+    GTK_SAT_MOD_LAYOUT_4      /*!< Three views, big one on bottom, small ones on top */
 } gtk_sat_mod_layout_t;
 
 
 /** \brief View types */
 typedef enum {
-	GTK_SAT_MOD_VIEW_LIST = 0,  /*!< GtkSatList */
-	GTK_SAT_MOD_VIEW_MAP,       /*!< GtkSatMap */
-	GTK_SAT_MOD_VIEW_POLAR,     /*!< GtkPolarView */
-	GTK_SAT_MOD_VIEW_SINGLE,    /*!< GtkSingleSat */
-	GTK_SAT_MOD_VIEW_MET,       /*!< GtkMet */
+    GTK_SAT_MOD_VIEW_LIST = 0,  /*!< GtkSatList */
+    GTK_SAT_MOD_VIEW_MAP,       /*!< GtkSatMap */
+    GTK_SAT_MOD_VIEW_POLAR,     /*!< GtkPolarView */
+    GTK_SAT_MOD_VIEW_SINGLE,    /*!< GtkSingleSat */
+    GTK_SAT_MOD_VIEW_MET,       /*!< GtkMet */
 } gtk_sat_mod_view_t;
 
 
 
 #define GTK_TYPE_SAT_MODULE         (gtk_sat_module_get_type ())
 #define GTK_SAT_MODULE(obj)         GTK_CHECK_CAST (obj,\
-									gtk_sat_module_get_type (),	\
-						            GtkSatModule)
+                                    gtk_sat_module_get_type (),	\
+                                    GtkSatModule)
 
 #define GTK_SAT_MODULE_CLASS(klass) GTK_CHECK_CLASS_CAST (klass,\
-							        gtk_sat_module_get_type (),\
-							        GtkSatModuleClass)
+gtk_sat_module_get_type (),\
+GtkSatModuleClass)
 
 #define IS_GTK_SAT_MODULE(obj)      GTK_CHECK_TYPE (obj, gtk_sat_module_get_type ())
 
@@ -85,86 +85,86 @@ typedef struct _GtkSatModuleClass   GtkSatModuleClass;
 
 struct _gtk_sat_module
 {
-	GtkVBox vbox;
+    GtkVBox vbox;
 
-	gchar         *name;         /*!< The module name */
+    gchar         *name;         /*!< The module name */
 
-	GtkWidget     *popup_button; /*!< popup menu button. */
-	GtkWidget     *close_button; /*!< The close button */
+    GtkWidget     *popup_button; /*!< popup menu button. */
+    GtkWidget     *close_button; /*!< The close button */
 
-	GtkWidget     *win;       /*!< Window when module is not docked */
+    GtkWidget     *win;       /*!< Window when module is not docked */
 
     GtkWidget     *rotctrlwin;  /*!< Rotator controller window */
     GtkWidget     *rotctrl;
     GtkWidget     *rigctrlwin;  /*!< Radio controller window */
     GtkWidget     *rigctrl;
 
-	GtkWidget     *header;
-	guint          head_count;
-	guint          head_timeout;
-	guint          event_count;
-	guint          event_timeout;
+    GtkWidget     *header;
+    guint          head_count;
+    guint          head_timeout;
+    guint          event_count;
+    guint          event_timeout;
 
-	/* layout */
-	GtkWidget     *vpaned;      /*!< Vertical space divider */
-	GtkWidget     *hpaned;      /*!< Horizontal space divider */
+    /* layout */
+    GtkWidget     *vpaned;      /*!< Vertical space divider */
+    GtkWidget     *hpaned;      /*!< Horizontal space divider */
 
-	/* these are needed when docking */
-	gint           vpanedpos;   /*!< Stored position of vpaned */
-	gint           hpanedpos;   /*!< Stored pos of hpaned */
+    /* these are needed when docking */
+    gint           vpanedpos;   /*!< Stored position of vpaned */
+    gint           hpanedpos;   /*!< Stored pos of hpaned */
 
-	/* children */
-	GtkWidget     *child_1;
-	GtkWidget     *child_2;
-	GtkWidget     *child_3;
+    /* children */
+    GtkWidget     *child_1;
+    GtkWidget     *child_2;
+    GtkWidget     *child_3;
 
-	GKeyFile      *cfgdata;      /*!< Configuration data. */
-	qth_t         *qth;          /*!< QTH information. */
-	
-	GHashTable    *satellites;   /*!< Satellites. */
+    GKeyFile      *cfgdata;      /*!< Configuration data. */
+    qth_t         *qth;          /*!< QTH information. */
 
-	guint32        timeout;      /*!< Timeout value [msec] */
+    GHashTable    *satellites;   /*!< Satellites. */
 
-	gtk_sat_mod_state_t  state;   /*!< The state of the module. */
-	gtk_sat_mod_layout_t layout;  /*!< module layout */
-	gtk_sat_mod_view_t   view_1;
-	gtk_sat_mod_view_t   view_2;
-	gtk_sat_mod_view_t   view_3;
+    guint32        timeout;      /*!< Timeout value [msec] */
 
-	guint          timerid;      /*!< The timeout ID (FIXME: REMOVE) */
-	gboolean       busy;         /*!< Flag indicating whether timeout has
-						   		      finished or not. Also used for blocking
-									  the module during TLE update.
-								 */
+    gtk_sat_mod_state_t  state;   /*!< The state of the module. */
+    gtk_sat_mod_layout_t layout;  /*!< module layout */
+    gtk_sat_mod_view_t   view_1;
+    gtk_sat_mod_view_t   view_2;
+    gtk_sat_mod_view_t   view_3;
 
-	/* time keeping */
-	gdouble        rtNow;        /*!< Real-time in this cycle */
-	gdouble        rtPrev;       /*!< Real-time in previous cycle */
+    guint          timerid;      /*!< The timeout ID (FIXME: REMOVE) */
+    gboolean       busy;         /*!< Flag indicating whether timeout has
+                                                                      finished or not. Also used for blocking
+                                                                          the module during TLE update.
+                                                                 */
 
-	gint           throttle;     /*!< Time throttle. */
-	gdouble        tmgPdnum;     /*!< Daynum at previous update. */
-	gdouble        tmgCdnum;     /*!< Daynum at current update. */
-	gboolean       tmgActive;    /*!< Flag indicating whether time mgr is active */
-	GtkWidget     *tmgFactor;    /*!< Spin button for throttle value selection 2..10 */
-	GtkWidget     *tmgCal;       /*!< Calendar widget for selecting date */
-	GtkWidget     *tmgHour;      /*!< Spin button for setting the hour */
-	GtkWidget     *tmgMin;       /*!< Spin button for setting the minutes */
-	GtkWidget     *tmgSec;       /*!< Spin button for setting the seconds */
-	GtkWidget     *tmgMsec;      /*!< Spin button for setting the milliseconds */
-	GtkWidget     *tmgSlider;    /*!< Slider for manual time "dragging" */
-	GtkWidget     *tmgStop;      /*!< Stop button; throttle = 0 */
-	GtkWidget     *tmgFwd;       /*!< Forward */
-	GtkWidget     *tmgBwd;       /*!< Backward */
-	GtkWidget     *tmgReset;     /*!< Reset button */
-	GtkWidget     *tmgWin;       /*!< Window containing the widgets. */
-	GtkWidget     *tmgState;     /*!< Status label indicating RT/SRT/MAN */
+    /* time keeping */
+    gdouble        rtNow;        /*!< Real-time in this cycle */
+    gdouble        rtPrev;       /*!< Real-time in previous cycle */
 
-	gboolean       reset;     /*!< Flag indicating whether time reset is in progress */
+    gint           throttle;     /*!< Time throttle. */
+    gdouble        tmgPdnum;     /*!< Daynum at previous update. */
+    gdouble        tmgCdnum;     /*!< Daynum at current update. */
+    gboolean       tmgActive;    /*!< Flag indicating whether time mgr is active */
+    GtkWidget     *tmgFactor;    /*!< Spin button for throttle value selection 2..10 */
+    GtkWidget     *tmgCal;       /*!< Calendar widget for selecting date */
+    GtkWidget     *tmgHour;      /*!< Spin button for setting the hour */
+    GtkWidget     *tmgMin;       /*!< Spin button for setting the minutes */
+    GtkWidget     *tmgSec;       /*!< Spin button for setting the seconds */
+    GtkWidget     *tmgMsec;      /*!< Spin button for setting the milliseconds */
+    GtkWidget     *tmgSlider;    /*!< Slider for manual time "dragging" */
+    GtkWidget     *tmgStop;      /*!< Stop button; throttle = 0 */
+    GtkWidget     *tmgFwd;       /*!< Forward */
+    GtkWidget     *tmgBwd;       /*!< Backward */
+    GtkWidget     *tmgReset;     /*!< Reset button */
+    GtkWidget     *tmgWin;       /*!< Window containing the widgets. */
+    GtkWidget     *tmgState;     /*!< Status label indicating RT/SRT/MAN */
+
+    gboolean       reset;     /*!< Flag indicating whether time reset is in progress */
 };
 
 struct _GtkSatModuleClass
 {
-	GtkVBoxClass parent_class;
+    GtkVBoxClass parent_class;
 };
 
 
