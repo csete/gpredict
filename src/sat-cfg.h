09e28029f475e4ae55d39d4787db97d514248380
csete <csete@ab6734ba-2843-0410-b83b-99c217ef6c0f>
2009-12-01 10:33:10 +0000

    Added configuration keys for GtkEventList.
    git-svn-id: https://gpredict.svn.sourceforge.net/svnroot/gpredict/trunk@535 ab6734ba-2843-0410-b83b-99c217ef6c0f

--- src/sat-cfg.h
+++ src/sat-cfg.h
@@ -95,6 +95,7 @@ typedef enum {
     SAT_CFG_INT_POLAR_INFO_COL,       /*!< Info colour. */
     SAT_CFG_INT_SINGLE_SAT_REFRESH,   /*!< Single-sat refresh rate (cycle). */
     SAT_CFG_INT_SINGLE_SAT_FIELDS,    /*!< Single-sat fields. */
+    SAT_CFG_INT_EVENT_LIST_REFRESH,   /*!< Event list refresh rate (cycle). */
     SAT_CFG_INT_PRED_MIN_EL,          /*!< Minimum elevation for passes. */
     SAT_CFG_INT_PRED_NUM_PASS,        /*!< Number of passes to predict. */
     SAT_CFG_INT_PRED_LOOK_AHEAD,      /*!< Look-ahead time limit in days. */ 
