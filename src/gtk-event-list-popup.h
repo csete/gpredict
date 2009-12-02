9f8e0bae969e7918026f257cd5e6d15585232bdf
csete <csete@ab6734ba-2843-0410-b83b-99c217ef6c0f>
2009-12-02 15:12:44 +0000

    Added code implementing pop-up menu for GtkEventList.
    git-svn-id: https://gpredict.svn.sourceforge.net/svnroot/gpredict/trunk@538 ab6734ba-2843-0410-b83b-99c217ef6c0f

---
+++ src/gtk-event-list-popup.h
@@ -0,0 +1,42 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*
+    Gpredict: Real-time satellite tracking and orbit prediction program
+
+    Copyright (C)  2001-2009  Alexandru Csete, OZ9AEC.
+
+    Authors: Alexandru Csete <oz9aec@gmail.com>
+
+    Comments, questions and bugreports should be submitted via
+    http://sourceforge.net/projects/gpredict/
+    More details can be found at the project home page:
+
+            http://gpredict.oz9aec.net/
+ 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+  
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+  
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, visit http://www.fsf.org/
+*/
+#ifndef GTK_EVENT_LIST_POPUP_H
+#define GTK_EVENT_LIST_POPUP_H 1
+
+#include <gtk/gtk.h>
+#include "sgpsdp/sgp4sdp4.h"
+#include "gtk-sat-data.h"
+#include "gtk-event-list.h"
+
+
+void gtk_event_list_popup_exec (sat_t *sat, qth_t *qth,
+                                GdkEventButton *event,
+                                GtkEventList *list);
+
+
+#endif
