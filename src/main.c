50d9962d2aa4c5c778d7a89341404a7bbdc9c12a
csete <csete@ab6734ba-2843-0410-b83b-99c217ef6c0f>
2010-01-02 17:57:34 +0000

    Fixed indentation.
    git-svn-id: https://gpredict.svn.sourceforge.net/svnroot/gpredict/trunk@569 ab6734ba-2843-0410-b83b-99c217ef6c0f

--- src/main.c
+++ src/main.c
@@ -143,14 +143,14 @@ int main (int argc, char *argv[])
 	
 #ifdef WIN32
 	// Initializing Windozze Sockets
-	InitWinSock2();
+    InitWinSock2();
 #endif
     
     gtk_main ();
 
 #ifdef WIN32
-	// Cleanup Windozze Sockets
-	CloseWinSock2();
+    // Cleanup Windozze Sockets
+    CloseWinSock2();
 #endif
     return 0;
 }
@@ -159,37 +159,37 @@ int main (int argc, char *argv[])
 /* This code was given from MSDN */
 static void InitWinSock2(void)
 {
-	WORD wVersionRequested;
-	WSADATA wsaData;
-	int err;
- 
-	wVersionRequested = MAKEWORD( 2, 2 );
- 
-	err = WSAStartup( wVersionRequested, &wsaData );
-	if ( err != 0 ) {
-		/* Tell the user that we could not find a usable */
-		/* WinSock DLL.                                  */
-		return;
-	}
- 
-	/* Confirm that the WinSock DLL supports 2.2.*/
-	/* Note that if the DLL supports versions later    */
-	/* than 2.2 in addition to 2.2, it will still return */
-	/* 2.2 in wVersion since that is the version we      */
-	/* requested.                                        */
- 
-	if ( LOBYTE( wsaData.wVersion ) != 2 ||
-			HIBYTE( wsaData.wVersion ) != 2 ) {
-		/* Tell the user that we could not find a usable */
-		/* WinSock DLL.                                  */
-		WSACleanup( );
-		return; 
-	}
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    int err;
+
+    wVersionRequested = MAKEWORD( 2, 2 );
+
+    err = WSAStartup( wVersionRequested, &wsaData );
+    if ( err != 0 ) {
+        /* Tell the user that we could not find a usable */
+        /* WinSock DLL.                                  */
+        return;
+    }
+
+    /* Confirm that the WinSock DLL supports 2.2.*/
+    /* Note that if the DLL supports versions later    */
+    /* than 2.2 in addition to 2.2, it will still return */
+    /* 2.2 in wVersion since that is the version we      */
+    /* requested.                                        */
+
+    if ( LOBYTE( wsaData.wVersion ) != 2 ||
+         HIBYTE( wsaData.wVersion ) != 2 ) {
+        /* Tell the user that we could not find a usable */
+        /* WinSock DLL.                                  */
+        WSACleanup( );
+        return;
+    }
 }
 
 static void CloseWinSock2(void)
 {
-	WSACleanup();
+    WSACleanup();
 }
 #endif
 
