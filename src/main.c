f559fbfd51435f3da91972991249e5fc9d66c161
csete <csete@ab6734ba-2843-0410-b83b-99c217ef6c0f>
2010-06-18 09:49:11 +0000

    Applied patch 3009725: Delete Explicit Call to gtk_set_locale.
    git-svn-id: https://gpredict.svn.sourceforge.net/svnroot/gpredict/trunk@621 ab6734ba-2843-0410-b83b-99c217ef6c0f

--- src/main.c
+++ src/main.c
@@ -120,7 +120,6 @@ int main (int argc, char *argv[])
     bind_textdomain_codeset (PACKAGE, "UTF-8");
     textdomain (PACKAGE);
 #endif
-    gtk_set_locale ();
     gtk_init (&argc, &argv);
 
     context = g_option_context_new ("");
