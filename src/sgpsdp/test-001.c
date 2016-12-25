57a3a3d0bb22bace71ca37cf0e48c6c6758f7fe7
Alexandru Csete <oz9aec@gmail.com>
2016-12-25 01:59:12 +0100

    Fix unit tests
    - Fix compile warnings
    - Indent
    - Remove obsolete email address

--- src/sgpsdp/test-001.c
+++ src/sgpsdp/test-001.c
@@ -4,8 +4,6 @@
 
     Copyright (C)  2001-2008  Alexandru Csete.
 
-    Authors: Alexandru Csete <csete@users.sourceforge.net>
-
     Comments, questions and bugreports should be submitted via
     http://sourceforge.net/projects/gpredict/
     More details can be found at the project home page:
@@ -29,9 +27,7 @@
       Boston, MA  02111-1307
       USA
 */
-/** \defgroup test001 Unit test for SGP4
- *  \ingroup tests
- */
+/* Unit test for SGP4 */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -42,110 +38,129 @@
 
 /* structure to hold a set of data */
 typedef struct {
-    double t;
-    double x;
-    double y;
-    double z;
-    double vx;
-    double vy;
-    double vz;
+    double          t;
+    double          x;
+    double          y;
+    double          z;
+    double          vx;
+    double          vy;
+    double          vz;
 } dataset_t;
 
 
 const dataset_t expected[TEST_STEPS] = {
-    { 0.0,
-      2328.97048951, -5995.22076416, 1719.97067261,
-      2.91207230, -0.98341546, -7.09081703 },
-    { 360.0,
-      2456.10705566, -6071.93853760, 1222.89727783,
-      2.67938992, -0.44829041, -7.22879231 },
-    { 720.0,
-      2567.56195068, -6112.50384522, 713.96397400,
-      2.44024599, 0.09810869, -7.31995916 },
-    { 1080.0,
-      2663.09078980, -6115.48229980, 196.39640427,
-      2.19611958, 0.65241995, -7.36282432 },
-    { 1440.0,
-      2742.55133057, -6079.67144775, -326.38095856,
-      1.94850229, 1.21106251, -7.35619372}
+    {0.0,
+     2328.97048951, -5995.22076416, 1719.97067261,
+     2.91207230, -0.98341546, -7.09081703},
+    {360.0,
+     2456.10705566, -6071.93853760, 1222.89727783,
+     2.67938992, -0.44829041, -7.22879231},
+    {720.0,
+     2567.56195068, -6112.50384522, 713.96397400,
+     2.44024599, 0.09810869, -7.31995916},
+    {1080.0,
+     2663.09078980, -6115.48229980, 196.39640427,
+     2.19611958, 0.65241995, -7.36282432},
+    {1440.0,
+     2742.55133057, -6079.67144775, -326.38095856,
+     1.94850229, 1.21106251, -7.35619372}
 };
 
 
-char   tle_str[3][80];
-sat_t  sat;
+char            tle_str[3][80];
+sat_t           sat;
 
-int
-main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-    FILE *fp;
-    char *b;
-    int i;
+    FILE           *fp;
+    int             i;
 
     /* read tle file */
-    fp = fopen ("test-001.tle", "r");
-    if (fp != NULL) {
- 
-        b = fgets (tle_str[0], 80, fp);
-        b = fgets (tle_str[1], 80, fp);
-        b = fgets (tle_str[2], 80, fp);
-        fclose (fp);
-
-        if (Get_Next_Tle_Set (tle_str, &sat.tle) == 1) {
-            printf ("TEST DATA:\n");
+    fp = fopen("test-001.tle", "r");
+    if (fp != NULL)
+    {
+        if (fgets(tle_str[0], 80, fp) == NULL)
+        {
+            printf("Error reading TLE line 1\n");
+            fclose(fp);
+            return 1;
         }
-        else {
+        if (fgets(tle_str[1], 80, fp) == NULL)
+        {
+            printf("Error reading TLE line 2\n");
+            fclose(fp);
+            return 1;
+        }
+        if (fgets(tle_str[2], 80, fp) == NULL)
+        {
+            printf("Error reading TLE line 3\n");
+            fclose(fp);
+            return 1;
+        }
+        fclose(fp);
+
+        if (Get_Next_Tle_Set(tle_str, &sat.tle) == 1)
+        {
+            printf("TEST DATA:\n");
+        }
+        else
+        {
             printf("Could not read TLE data 1\n");
             return 1;
         }
     }
-    else {
+    else
+    {
         printf("Could not open test-001.tle\n");
         return 1;
     }
-    
-    printf ("%s", tle_str[0]);
-    printf ("%s", tle_str[1]);
-    printf ("%s", tle_str[2]);
-    
-    select_ephemeris (&sat);
 
-    printf ("\nDEEP_SPACE_EPHEM: %d (expected 0)\n\n",
-        (sat.flags & DEEP_SPACE_EPHEM_FLAG));
+    printf("%s", tle_str[0]);
+    printf("%s", tle_str[1]);
+    printf("%s", tle_str[2]);
+
+    select_ephemeris(&sat);
+
+    printf("\nDEEP_SPACE_EPHEM: %d (expected 0)\n\n",
+           (sat.flags & DEEP_SPACE_EPHEM_FLAG));
 
 
-    printf ("                         RESULT          EXPECTED            DELTA\n");
-    printf ("------------------------------------------------------------------------------\n");
+    printf("                         RESULT          EXPECTED          "
+           "  DELTA\n");
+    printf("-----------------------------------------------------------"
+           "-------------------\n");
 
-    for (i = 0; i < TEST_STEPS; i++) {
-    
-        SGP4 (&sat, expected[i].t);
-        Convert_Sat_State (&sat.pos, &sat.vel);
+    for (i = 0; i < TEST_STEPS; i++)
+    {
 
-        printf ("STEP %d  t: %6.1f  X: %14.8f   %14.8f   %.8f (%.5f%%)\n",
-            i+1, expected[i].t, sat.pos.x, expected[i].x,
-            fabs (sat.pos.x - expected[i].x),
-            100.0 * fabs (sat.pos.x - expected[i].x) / fabs (expected[i].x));
-        printf ("                   Y: %14.8f   %14.8f   %.8f (%.5f%%)\n",
-            sat.pos.y, expected[i].y,
-            fabs (sat.pos.y - expected[i].y),
-            100.0 * fabs (sat.pos.y - expected[i].y) / fabs (expected[i].y));
-        printf ("                   Z: %14.8f   %14.8f   %.8f (%.5f%%)\n",
-            sat.pos.z, expected[i].z,
-            fabs (sat.pos.z - expected[i].z),
-            100.0 * fabs (sat.pos.z - expected[i].z) / fabs (expected[i].z));
-        printf ("                  VX: %14.8f   %14.8f   %.8f (%.5f%%)\n",
-            sat.vel.x, expected[i].vx,
-            fabs (sat.vel.x - expected[i].vx),
-            100.0 * fabs (sat.vel.x - expected[i].vx) / fabs (expected[i].vx));
-        printf ("                  VY: %14.8f   %14.8f   %.8f (%.5f%%)\n",
-            sat.vel.y, expected[i].vy,
-            fabs (sat.vel.y - expected[i].vy),
-            100.0 * fabs (sat.vel.y - expected[i].vy) / fabs (expected[i].vy));
-        printf ("                  VZ: %14.8f   %14.8f   %.8f (%.5f%%)\n",
-            sat.vel.z, expected[i].vz,
-            fabs (sat.vel.z - expected[i].vz),
-            100.0 * fabs (sat.vel.z - expected[i].vz) / fabs (expected[i].vz));
+        SGP4(&sat, expected[i].t);
+        Convert_Sat_State(&sat.pos, &sat.vel);
 
+        printf("STEP %d  t: %6.1f  X: %14.8f   %14.8f   %.8f (%.5f%%)\n",
+               i + 1, expected[i].t, sat.pos.x, expected[i].x,
+               fabs(sat.pos.x - expected[i].x),
+               100.0 * fabs(sat.pos.x - expected[i].x) / fabs(expected[i].x));
+        printf("                   Y: %14.8f   %14.8f   %.8f (%.5f%%)\n",
+               sat.pos.y, expected[i].y,
+               fabs(sat.pos.y - expected[i].y),
+               100.0 * fabs(sat.pos.y - expected[i].y) / fabs(expected[i].y));
+        printf("                   Z: %14.8f   %14.8f   %.8f (%.5f%%)\n",
+               sat.pos.z, expected[i].z,
+               fabs(sat.pos.z - expected[i].z),
+               100.0 * fabs(sat.pos.z - expected[i].z) / fabs(expected[i].z));
+        printf("                  VX: %14.8f   %14.8f   %.8f (%.5f%%)\n",
+               sat.vel.x, expected[i].vx,
+               fabs(sat.vel.x - expected[i].vx),
+               100.0 * fabs(sat.vel.x -
+                            expected[i].vx) / fabs(expected[i].vx));
+        printf("                  VY: %14.8f   %14.8f   %.8f (%.5f%%)\n",
+               sat.vel.y, expected[i].vy, fabs(sat.vel.y - expected[i].vy),
+               100.0 * fabs(sat.vel.y -
+                            expected[i].vy) / fabs(expected[i].vy));
+        printf("                  VZ: %14.8f   %14.8f   %.8f (%.5f%%)\n",
+               sat.vel.z, expected[i].vz, fabs(sat.vel.z - expected[i].vz),
+               100.0 * fabs(sat.vel.z -
+                            expected[i].vz) / fabs(expected[i].vz));
     }
 
     return 0;
--- src/sgpsdp/test-002.c
+++ src/sgpsdp/test-002.c
@@ -4,8 +4,6 @@
 
     Copyright (C)  2001-2008  Alexandru Csete.
 
-    Authors: Alexandru Csete <csete@users.sourceforge.net>
-
     Comments, questions and bugreports should be submitted via
     http://sourceforge.net/projects/gpredict/
     More details can be found at the project home page:
@@ -29,9 +27,7 @@
       Boston, MA  02111-1307
       USA
 */
-/** \defgroup test001 Unit test for SGP4
- *  \ingroup tests
- */
+/* Unit test for SGP4 */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -42,110 +38,128 @@
 
 /* structure to hold a set of data */
 typedef struct {
-    double t;
-    double x;
-    double y;
-    double z;
-    double vx;
-    double vy;
-    double vz;
+    double          t;
+    double          x;
+    double          y;
+    double          z;
+    double          vx;
+    double          vy;
+    double          vz;
 } dataset_t;
 
 
 const dataset_t expected[TEST_STEPS] = {
-    { 0.0,
-      7473.37066650, 428.95261765, 5828.74786377,
-      5.1071513, 6.44468284, -0.18613096 },
-    { 360.0,
-      -3305.22537232, 32410.86328125, -24697.17675781,
-      -1.30113538, -1.15131518, -0.28333528 },
-    { 720.0,
-      14271.28759766, 24110.46411133, -4725.76837158,
-      -0.32050445, 2.67984074, -2.08405289 },
-    { 1080.0,
-      -9990.05883789, 22717.35522461, -23616.890662501,
-      -1.01667246, -2.29026759, 0.72892364 },
-    { 1440.0,
-      9787.86975097, 33753.34667969, -15030.81176758,
-      -1.09425966, 0.92358845, -1.52230928}
+    {0.0,
+     7473.37066650, 428.95261765, 5828.74786377,
+     5.1071513, 6.44468284, -0.18613096},
+    {360.0,
+     -3305.22537232, 32410.86328125, -24697.17675781,
+     -1.30113538, -1.15131518, -0.28333528},
+    {720.0,
+     14271.28759766, 24110.46411133, -4725.76837158,
+     -0.32050445, 2.67984074, -2.08405289},
+    {1080.0,
+     -9990.05883789, 22717.35522461, -23616.890662501,
+     -1.01667246, -2.29026759, 0.72892364},
+    {1440.0,
+     9787.86975097, 33753.34667969, -15030.81176758,
+     -1.09425966, 0.92358845, -1.52230928}
 };
 
 
-char   tle_str[3][80];
-sat_t  sat;
+char            tle_str[3][80];
+sat_t           sat;
 
-int
-main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-    FILE *fp;
-    char *b;
-    int i;
+    FILE           *fp;
+    int             i;
 
     /* read tle file */
-    fp = fopen ("test-002.tle", "r");
-    if (fp != NULL) {
- 
-        b = fgets (tle_str[0], 80, fp);
-        b = fgets (tle_str[1], 80, fp);
-        b = fgets (tle_str[2], 80, fp);
-        fclose (fp);
-
-        if (Get_Next_Tle_Set (tle_str, &sat.tle) == 1) {
-            printf ("TEST DATA:\n");
+    fp = fopen("test-002.tle", "r");
+    if (fp != NULL)
+    {
+        if (fgets(tle_str[0], 80, fp) == NULL)
+        {
+            printf("Error reading TLE line 1\n");
+            fclose(fp);
+            return 1;
         }
-        else {
+        if (fgets(tle_str[1], 80, fp) == NULL)
+        {
+            printf("Error reading TLE line 2\n");
+            fclose(fp);
+            return 1;
+        }
+        if (fgets(tle_str[2], 80, fp) == NULL)
+        {
+            printf("Error reading TLE line 3\n");
+            fclose(fp);
+            return 1;
+        }
+        fclose(fp);
+
+        if (Get_Next_Tle_Set(tle_str, &sat.tle) == 1)
+        {
+            printf("TEST DATA:\n");
+        }
+        else
+        {
             printf("Could not read TLE data 2\n");
             return 1;
         }
     }
-    else {
+    else
+    {
         printf("Could not open test-001.tle\n");
         return 1;
     }
-    
-    printf ("%s", tle_str[0]);
-    printf ("%s", tle_str[1]);
-    printf ("%s", tle_str[2]);
 
-    select_ephemeris (&sat);
+    printf("%s", tle_str[0]);
+    printf("%s", tle_str[1]);
+    printf("%s", tle_str[2]);
 
-    printf ("\nDEEP_SPACE_EPHEM: %d (expected %d)\n\n",
-        (sat.flags & DEEP_SPACE_EPHEM_FLAG), DEEP_SPACE_EPHEM_FLAG);
+    select_ephemeris(&sat);
+
+    printf("\nDEEP_SPACE_EPHEM: %d (expected %d)\n\n",
+           (sat.flags & DEEP_SPACE_EPHEM_FLAG), DEEP_SPACE_EPHEM_FLAG);
 
 
-    printf ("                          RESULT            EXPECTED            DELTA\n");
-    printf ("---------------------------------------------------------------------------------\n");
+    printf("                          RESULT            EXPECTED       "
+           "     DELTA\n");
+    printf("-----------------------------------------------------------"
+           "----------------------\n");
 
-    for (i = 0; i < TEST_STEPS; i++) {
-    
-        SDP4 (&sat, expected[i].t);
-        Convert_Sat_State (&sat.pos, &sat.vel);
+    for (i = 0; i < TEST_STEPS; i++)
+    {
+        SDP4(&sat, expected[i].t);
+        Convert_Sat_State(&sat.pos, &sat.vel);
 
-        printf ("STEP %d  t: %6.1f  X: %15.8f   %15.8f   %.8f (%.5f%%)\n",
-            i+1, expected[i].t, sat.pos.x, expected[i].x,
-            fabs (sat.pos.x - expected[i].x),
-            100.0 * fabs (sat.pos.x - expected[i].x) / fabs (expected[i].x));
-        printf ("                   Y: %15.8f   %15.8f   %.8f (%.5f%%)\n",
-            sat.pos.y, expected[i].y,
-            fabs (sat.pos.y - expected[i].y),
-            100.0 * fabs (sat.pos.y - expected[i].y) / fabs (expected[i].y));
-        printf ("                   Z: %15.8f   %15.8f   %.8f (%.5f%%)\n",
-            sat.pos.z, expected[i].z,
-            fabs (sat.pos.z - expected[i].z),
-            100.0 * fabs (sat.pos.z - expected[i].z) / fabs (expected[i].z));
-        printf ("                  VX: %15.8f   %15.8f   %.8f (%.5f%%)\n",
-            sat.vel.x, expected[i].vx,
-            fabs (sat.vel.x - expected[i].vx),
-            100.0 * fabs (sat.vel.x - expected[i].vx) / fabs (expected[i].vx));
-        printf ("                  VY: %15.8f   %15.8f   %.8f (%.5f%%)\n",
-            sat.vel.y, expected[i].vy,
-            fabs (sat.vel.y - expected[i].vy),
-            100.0 * fabs (sat.vel.y - expected[i].vy) / fabs (expected[i].vy));
-        printf ("                  VZ: %15.8f   %15.8f   %.8f (%.5f%%)\n",
-            sat.vel.z, expected[i].vz,
-            fabs (sat.vel.z - expected[i].vz),
-            100.0 * fabs (sat.vel.z - expected[i].vz) / fabs (expected[i].vz));
-
+        printf("STEP %d  t: %6.1f  X: %15.8f   %15.8f   %.8f (%.5f%%)\n",
+               i + 1, expected[i].t, sat.pos.x, expected[i].x,
+               fabs(sat.pos.x - expected[i].x),
+               100.0 * fabs(sat.pos.x - expected[i].x) / fabs(expected[i].x));
+        printf("                   Y: %15.8f   %15.8f   %.8f (%.5f%%)\n",
+               sat.pos.y, expected[i].y,
+               fabs(sat.pos.y - expected[i].y),
+               100.0 * fabs(sat.pos.y - expected[i].y) / fabs(expected[i].y));
+        printf("                   Z: %15.8f   %15.8f   %.8f (%.5f%%)\n",
+               sat.pos.z, expected[i].z,
+               fabs(sat.pos.z - expected[i].z),
+               100.0 * fabs(sat.pos.z - expected[i].z) / fabs(expected[i].z));
+        printf("                  VX: %15.8f   %15.8f   %.8f (%.5f%%)\n",
+               sat.vel.x, expected[i].vx,
+               fabs(sat.vel.x - expected[i].vx),
+               100.0 * fabs(sat.vel.x -
+                            expected[i].vx) / fabs(expected[i].vx));
+        printf("                  VY: %15.8f   %15.8f   %.8f (%.5f%%)\n",
+               sat.vel.y, expected[i].vy, fabs(sat.vel.y - expected[i].vy),
+               100.0 * fabs(sat.vel.y -
+                            expected[i].vy) / fabs(expected[i].vy));
+        printf("                  VZ: %15.8f   %15.8f   %.8f (%.5f%%)\n",
+               sat.vel.z, expected[i].vz, fabs(sat.vel.z - expected[i].vz),
+               100.0 * fabs(sat.vel.z -
+                            expected[i].vz) / fabs(expected[i].vz));
     }
 
     return 0;
