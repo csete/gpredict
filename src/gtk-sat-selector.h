75e3b477fbe50f5733585b8e45c038b8c3797491
Alexandru Csete <oz9aec@gmail.com>
2017-03-24 23:34:21 +0100

    Indent

--- src/gtk-sat-selector.h
+++ src/gtk-sat-selector.h
@@ -29,87 +29,84 @@
 #define __GTK_SAT_SELECTOR_H__ 1
 
 #include <gtk/gtk.h>
-//#include "gtk-sat-list.h"
 
-
+/* *INDENT-OFF* */
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
+/* *INDENT-ON* */
 
 
-/** \brief Column definitions in the tree. */
+/** @brief Column definitions in the tree. */
 typedef enum {
-    GTK_SAT_SELECTOR_COL_NAME = 0,  /*!< Satellite name. */
-    GTK_SAT_SELECTOR_COL_CATNUM,    /*!< Catalogue Number. */
-    GTK_SAT_SELECTOR_COL_EPOCH,     /*!< Element set epoch. */
-    GTK_SAT_SELECTOR_COL_SELECTED,  /*!< Track whether element is selected. */
-    GTK_SAT_SELECTOR_COL_NUM        /*!< The number of columns. */
+    GTK_SAT_SELECTOR_COL_NAME = 0,      /*!< Satellite name. */
+    GTK_SAT_SELECTOR_COL_CATNUM,        /*!< Catalogue Number. */
+    GTK_SAT_SELECTOR_COL_EPOCH, /*!< Element set epoch. */
+    GTK_SAT_SELECTOR_COL_SELECTED,      /*!< Track whether element is selected. */
+    GTK_SAT_SELECTOR_COL_NUM    /*!< The number of columns. */
 } gtk_sat_selector_col_t;
 
-
-/** \brief Flags used to indicate which columns should be visible. */
+/** @brief Flags used to indicate which columns should be visible. */
 typedef enum {
-    GTK_SAT_SELECTOR_FLAG_NAME   = 1 << GTK_SAT_SELECTOR_COL_NAME,   /*!< Satellite name. */
-    GTK_SAT_SELECTOR_FLAG_CATNUM = 1 << GTK_SAT_SELECTOR_COL_CATNUM, /*!< Catalogue Number. */
-    GTK_SAT_SELECTOR_FLAG_EPOCH  = 1 << GTK_SAT_SELECTOR_COL_EPOCH,  /*!< Element set epoch. */
-    GTK_SAT_SELECTOR_FLAG_SELECTED  = 1 << GTK_SAT_SELECTOR_COL_SELECTED,  /*!< Item selected or not. */
+    GTK_SAT_SELECTOR_FLAG_NAME = 1 << GTK_SAT_SELECTOR_COL_NAME,        /*!< Satellite name. */
+    GTK_SAT_SELECTOR_FLAG_CATNUM = 1 << GTK_SAT_SELECTOR_COL_CATNUM,    /*!< Catalogue Number. */
+    GTK_SAT_SELECTOR_FLAG_EPOCH = 1 << GTK_SAT_SELECTOR_COL_EPOCH,      /*!< Element set epoch. */
+    GTK_SAT_SELECTOR_FLAG_SELECTED = 1 << GTK_SAT_SELECTOR_COL_SELECTED,        /*!< Item selected or not. */
 } gtk_sat_selector_flag_t;
 
 
-
 #define GTK_SAT_SELECTOR_DEFAULT_FLAGS (GTK_SAT_SELECTOR_FLAG_NAME | GTK_SAT_SELECTOR_FLAG_CATNUM)
-
-
 #define GTK_TYPE_SAT_SELECTOR  (gtk_sat_selector_get_type ())
 #define GTK_SAT_SELECTOR(obj)  G_TYPE_CHECK_INSTANCE_CAST (obj,\
                                                gtk_sat_selector_get_type (),\
                                                GtkSatSelector)
-
 #define GTK_SAT_SELECTOR_CLASS(klass)  G_TYPE_CHECK_CLASS_CAST (klass,\
                                                              gtk_sat_selector_get_type (),\
                                                              GtkSatSelectorClass)
-
 #define IS_GTK_SAT_SELECTOR(obj)       G_TYPE_CHECK_INSTANCE_TYPE (obj, gtk_sat_selector_get_type ())
 
 
-/** \brief The GtkSatSelector structure */
-typedef struct _gtk_sat_selector  GtkSatSelector;
+/** @brief The GtkSatSelector structure */
+typedef struct _gtk_sat_selector GtkSatSelector;
 typedef struct _GtkSatSelectorClass GtkSatSelectorClass;
 
 
-/** \brief The GtkSatSelector Structure definition */
-struct _gtk_sat_selector
-{
-    GtkVBox vbox;
+/** @brief The GtkSatSelector Structure definition */
+struct _gtk_sat_selector {
+    GtkVBox         vbox;
 
-    GtkWidget   *tree;        /*!< The tree. */
-    GtkWidget   *swin;        /*!< Scrolled window. */
-    guint        flags;       /*!< Column visibility flags. */
+    GtkWidget      *tree;       /*!< The tree. */
+    GtkWidget      *swin;       /*!< Scrolled window. */
+    guint           flags;      /*!< Column visibility flags. */
 
-    GtkWidget   *groups;      /*!< Combo box for selecting satellite group. */
-    GtkWidget   *search;      /*!< Text entry for searching. */
-    GSList      *models;      /*!< List of models with index corresponding to groups. */
+    GtkWidget      *groups;     /*!< Combo box for selecting satellite group. */
+    GtkWidget      *search;     /*!< Text entry for searching. */
+    GSList         *models;     /*!< List of models with index corresponding to groups. */
 };
 
-struct _GtkSatSelectorClass
-{
-    GtkVBoxClass parent_class;
+struct _GtkSatSelectorClass {
+    GtkVBoxClass    parent_class;
 
-    void (* gtksatselector) (GtkSatSelector *sel);
+    void            (*gtksatselector) (GtkSatSelector * sel);
 };
 
 
-GType      gtk_sat_selector_get_type  (void);
-GtkWidget *gtk_sat_selector_new       (guint flags);
-guint32    gtk_sat_selector_get_flags (GtkSatSelector *selector);
-void       gtk_sat_selector_get_selected (GtkSatSelector *selector, gint *catnum, gchar **satname, gdouble *epoch);
-gdouble    gtk_sat_selector_get_latest_epoch (GtkSatSelector *selector);
-void       gtk_sat_selector_mark_selected (GtkSatSelector *selector, gint catnum);
-void       gtk_sat_selector_mark_unselected (GtkSatSelector *selector, gint catnum);
+GType           gtk_sat_selector_get_type(void);
+GtkWidget      *gtk_sat_selector_new(guint flags);
+guint32         gtk_sat_selector_get_flags(GtkSatSelector * selector);
+void            gtk_sat_selector_get_selected(GtkSatSelector * selector,
+                                              gint * catnum, gchar ** satname,
+                                              gdouble * epoch);
+gdouble         gtk_sat_selector_get_latest_epoch(GtkSatSelector * selector);
+void            gtk_sat_selector_mark_selected(GtkSatSelector * selector,
+                                               gint catnum);
+void            gtk_sat_selector_mark_unselected(GtkSatSelector * selector,
+                                                 gint catnum);
 
+/* *INDENT-OFF* */
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
-
+/* *INDENT-ON* */
 
 #endif
