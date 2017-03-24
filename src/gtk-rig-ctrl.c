4d0fbc8a6f1c3d7340306f91829487911d4dc42a
Alexandru Csete <oz9aec@gmail.com>
2017-03-24 22:57:45 +0100

    Remove deprecated Gdk API calls.
    Issue #1

--- src/gtk-rig-ctrl.c
+++ src/gtk-rig-ctrl.c
@@ -159,11 +159,6 @@ static void     start_timer(GtkRigCtrl * data);
 
 static GtkVBoxClass *parent_class = NULL;
 
-static GdkColor ColBlack = { 0, 0, 0, 0 };
-static GdkColor ColWhite = { 0, 0xFFFF, 0xFFFF, 0xFFFF };
-static GdkColor ColRed = { 0, 0xFFFF, 0, 0 };
-static GdkColor ColGreen = { 0, 0, 0xFFFF, 0 };
-
 
 GType gtk_rig_ctrl_get_type()
 {
@@ -316,11 +311,6 @@ GtkWidget      *gtk_rig_ctrl_new(GtkSatModule * module)
             get_next_pass(GTK_RIG_CTRL(widget)->target,
                           GTK_RIG_CTRL(widget)->qth, 3.0);
     }
-    /* initialise custom colors */
-    gdk_rgb_find_color(gtk_widget_get_colormap(widget), &ColBlack);
-    gdk_rgb_find_color(gtk_widget_get_colormap(widget), &ColWhite);
-    gdk_rgb_find_color(gtk_widget_get_colormap(widget), &ColRed);
-    gdk_rgb_find_color(gtk_widget_get_colormap(widget), &ColGreen);
 
     /* create contents */
     table = gtk_table_new(3, 2, FALSE);
