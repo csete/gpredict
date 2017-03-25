csete/gpredict

commit 9c11d04cc440490915c5de24642489541e3947b7

Enable Print button in single pass dialog.
git-svn-id: https://gpredict.svn.sourceforge.net/svnroot/gpredict/trunk@590 ab6734ba-2843-0410-b83b-99c217ef6c0f

src/sat-pass-dialogs.c
@@ -46,6 +46,7 @@
 #include "gtk-polar-plot.h"
 #include "gtk-azel-plot.h"
 #include "save-pass.h"
+#include "print-pass.h"
 
 
 
@@ -546,6 +547,11 @@ static void single_pass_response (GtkWidget *dialog, gint response, gpointer dat
         sat_log_log (SAT_LOG_LEVEL_ERROR,
                      _("%s: PRINT not implemented"),
                      __FUNCTION__);
+
+        pass_t    *pass = (pass_t *) g_object_get_data (G_OBJECT (dialog), "pass");
+        qth_t     *qth = (qth_t *) g_object_get_data (G_OBJECT (dialog), "qth");
+
+        print_pass (pass, qth, GTK_WINDOW (dialog));
         break;
 
     case RESPONSE_SAVE:

commit f239274db1d08a367f36ca7e6d717d6e9f6a9501

Unexpected log message removed.
Fixes: 9c11d04 ("Enable Print button in single pass dialog.")

src/sat-pass-dialogs.c
@@ -542,16 +542,14 @@ void show_pass (const gchar *satname, qth_t *qth, pass_t *pass, GtkWidget *tople
 static void single_pass_response (GtkWidget *dialog, gint response, gpointer data)
 {
     (void) data; /* avoid unused parameter compiler warning */
+    pass_t *pass;
+    qth_t  *qth;
 
     switch (response) {
 
     case RESPONSE_PRINT:
-        sat_log_log (SAT_LOG_LEVEL_ERROR,
-                     _("%s: PRINT not implemented"),
-                     __func__);
-
-        pass_t    *pass = (pass_t *) g_object_get_data (G_OBJECT (dialog), "pass");
-        qth_t     *qth = (qth_t *) g_object_get_data (G_OBJECT (dialog), "qth");
+        pass = (pass_t *) g_object_get_data (G_OBJECT (dialog), "pass");
+        qth = (qth_t *) g_object_get_data (G_OBJECT (dialog), "qth");
 
         print_pass (pass, qth, GTK_WINDOW (dialog));
         break;