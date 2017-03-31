c5b16ec4b988d541cfff68fb46a656a0d82b4670
Alexandru Csete <oz9aec@gmail.com>
2017-03-31 14:52:23 +0200

    Indent gtk-freq-knob

--- src/gtk-freq-knob.h
+++ src/gtk-freq-knob.h
@@ -1,4 +1,3 @@
-/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
   Gpredict: Real-time satellite tracking and orbit prediction program
 
@@ -32,18 +31,17 @@
 #include <glib/gi18n.h>
 #include <gtk/gtk.h>
 
-
+/* *INDENT-OFF* */
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
-
-
+/* *INDENT-OO* */
 
 
 #define GTK_TYPE_FREQ_KNOB          (gtk_freq_knob_get_type ())
 #define GTK_FREQ_KNOB(obj)          G_TYPE_CHECK_INSTANCE_CAST (obj,\
-                                           gtk_freq_knob_get_type (),\
-                                             GtkFreqKnob)
+                                    gtk_freq_knob_get_type (),\
+                                    GtkFreqKnob)
 
 #define GTK_FREQ_KNOB_CLASS(klass)  G_TYPE_CHECK_CLASS_CAST (klass,\
                                     gtk_freq_knob_get_type (),\
@@ -54,8 +52,6 @@ extern "C" {
 
 typedef struct _gtk_freq_knob      GtkFreqKnob;
 typedef struct _GtkFreqKnobClass   GtkFreqKnobClass;
-
-
 
 struct _gtk_freq_knob
 {
@@ -75,18 +71,16 @@ struct _GtkFreqKnobClass
      GtkVBoxClass parent_class;
 };
 
-
-
 GType    gtk_freq_knob_get_type  (void);
 GtkWidget* gtk_freq_knob_new       (gdouble val, gboolean buttons);
 void       gtk_freq_knob_set_value (GtkFreqKnob *knob, gdouble val);
 gdouble    gtk_freq_knob_get_value (GtkFreqKnob *knob);
 
 
-
-
+/* *INDENT-OFF* */
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
+/* *INDENT-ON* */
 
 #endif /* __GTK_FREQ_KNOB_H__ */
