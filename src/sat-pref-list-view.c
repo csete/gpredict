fff1b7f6298baa29103923cdab2de782ec5aa77e
Alexandru Csete <oz9aec@gmail.com>
2017-12-24 17:40:48 +0100

    Migrate sat-pref-list-view to Gtk+ 3
    Issue #80.

--- src/sat-pref-list-view.c
+++ src/sat-pref-list-view.c
@@ -1,16 +1,8 @@
 /*
     Gpredict: Real-time satellite tracking and orbit prediction program
 
-    Copyright (C)  2001-2009  Alexandru Csete, OZ9AEC.
+    Copyright (C)  2001-2017  Alexandru Csete, OZ9AEC.
 
-    Authors: Alexandru Csete <oz9aec@gmail.com>
-
-    Comments, questions and bugreports should be submitted via
-    http://sourceforge.net/projects/gpredict/
-    More details can be found at the project home page:
-
-            http://gpredict.oz9aec.net/
- 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
@@ -36,11 +28,9 @@
 #include "sat-cfg.h"
 #include "sat-pref-list-view.h"
 
-/** First row where checkboxes are placed */
-#define Y0 1
 
-/** Number of columns in the table */
-#define COLUMNS 3
+#define Y0          1           /* First row where checkboxes are placed */
+#define COLUMNS     3           /* Number of columns in the table */
 
 /* column selector */
 static GtkWidget *check[SAT_LIST_COL_NUMBER];
@@ -51,102 +41,14 @@ static gboolean reset = FALSE;
 static guint    startflags;
 static guint    flags;
 
-static void     create_reset_button(GKeyFile * cfg, GtkBox * vbox);
-static void     reset_cb(GtkWidget * button, gpointer cfg);
-static void     toggle_cb(GtkToggleButton * toggle, gpointer data);
 
-/**
- * Create and initialise widgets for the list view preferences tab.
- *
- * The widgets must be preloaded with values from config. If a config value
- * is NULL, sensible default values, eg. those from defaults.h should
- * be loaded.
- */
-GtkWidget      *sat_pref_list_view_create(GKeyFile * cfg)
-{
-    GtkWidget      *vbox;
-    gint            i;
-    GtkWidget      *table, *label;
-
-    /* column visibility selector */
-    if (cfg != NULL)
-    {
-        flags = mod_cfg_get_int(cfg,
-                                MOD_CFG_LIST_SECTION,
-                                MOD_CFG_LIST_COLUMNS,
-                                SAT_CFG_INT_LIST_COLUMNS);
-    }
-    else
-    {
-        flags = sat_cfg_get_int(SAT_CFG_INT_LIST_COLUMNS);
-    }
-
-    /* create the table */
-    table = gtk_table_new((SAT_LIST_COL_NUMBER + 1) / COLUMNS + 1, COLUMNS,
-                          TRUE);
-    //gtk_container_set_border_width (GTK_CONTAINER (table), 20);
-    gtk_table_set_row_spacings(GTK_TABLE(table), 10);
-    gtk_table_set_col_spacings(GTK_TABLE(table), 5);
-
-    /* create header */
-    label = gtk_label_new(NULL);
-    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
-    gtk_label_set_markup(GTK_LABEL(label), _("<b>Visible Fields:</b>"));
-    gtk_table_attach(GTK_TABLE(table), label, 0, 2, 0, 1,
-                     GTK_FILL, GTK_SHRINK, 0, 0);
-
-    /* add fields except the last one (bold) */
-    for (i = 0; i < SAT_LIST_COL_NUMBER - 1; i++)
-    {
-        check[i] = gtk_check_button_new_with_label(_(SAT_LIST_COL_TITLE[i]));
-        gtk_widget_set_tooltip_text(check[i], _(SAT_LIST_COL_HINT[i]));
-        gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(check[i]),
-                                     flags & (1 << i));
-        gtk_table_attach(GTK_TABLE(table), check[i],
-                         i % COLUMNS, (i % COLUMNS) + 1,
-                         Y0 + i / COLUMNS, Y0 + i / COLUMNS + 1,
-                         GTK_FILL, GTK_SHRINK, 0, 0);
-        g_signal_connect(check[i], "toggled",
-                         G_CALLBACK(toggle_cb), GUINT_TO_POINTER(i));
-    }
-
-#if 0
-    colsel = gtk_sat_list_col_sel_new(columns);
-    frame1 = gtk_frame_new(_("Visible Columns"));
-    gtk_frame_set_label_align(GTK_FRAME(frame1), 0.5, 0.5);
-    gtk_container_add(GTK_CONTAINER(frame1), colsel);
-
-    /* Colours */
-    frame2 = gtk_frame_new("");
-    gtk_frame_set_label_align(GTK_FRAME(frame2), 0.5, 0.5);
-
-    hbox = gtk_hbox_new(FALSE, 5);
-    gtk_container_set_border_width(GTK_CONTAINER(hbox), 5);
-    gtk_box_pack_start(GTK_BOX(hbox), frame1, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(hbox), frame2, TRUE, TRUE, 0);
-    gtk_widget_show_all(hbox);
-#endif
-
-    vbox = gtk_vbox_new(FALSE, 10);
-    gtk_container_set_border_width(GTK_CONTAINER(vbox), 10);
-    gtk_box_pack_start(GTK_BOX(vbox), table, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), gtk_hseparator_new(), FALSE, FALSE, 0);
-    create_reset_button(cfg, GTK_BOX(vbox));
-
-    startflags = flags;
-    dirty = FALSE;
-    reset = FALSE;
-
-    return vbox;
-}
-
-/** User pressed cancel. Any changes to config must be cancelled. */
+/* User pressed cancel. Any changes to config must be cancelled. */
 void sat_pref_list_view_cancel(GKeyFile * cfg)
 {
     (void)cfg;
 }
 
-/** User pressed OK. Any changes should be stored in config. */
+/* User pressed OK. Any changes should be stored in config. */
 void sat_pref_list_view_ok(GKeyFile * cfg)
 {
     if (dirty)
@@ -180,52 +82,7 @@ void sat_pref_list_view_ok(GKeyFile * cfg)
     reset = FALSE;
 }
 
-
-/**
- * Create RESET button.
- * @param cfg Config data or NULL in global mode.
- * @param vbox The container.
- *
- * This function creates and sets up the view selector combos.
- */
-static void create_reset_button(GKeyFile * cfg, GtkBox * vbox)
-{
-    GtkWidget      *button;
-    GtkWidget      *butbox;
-
-    button = gtk_button_new_with_label(_("Reset"));
-    g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(reset_cb), cfg);
-
-    if (cfg == NULL)
-    {
-        gtk_widget_set_tooltip_text(button,
-                                    _
-                                    ("Reset settings to the default values."));
-    }
-    else
-    {
-        gtk_widget_set_tooltip_text(button,
-                                    _
-                                    ("Reset module settings to the global values."));
-    }
-
-    butbox = gtk_hbutton_box_new();
-    gtk_button_box_set_layout(GTK_BUTTON_BOX(butbox), GTK_BUTTONBOX_END);
-    gtk_box_pack_end(GTK_BOX(butbox), button, FALSE, TRUE, 10);
-
-    gtk_box_pack_end(vbox, butbox, FALSE, TRUE, 0);
-}
-
-/**
- * Reset settings.
- * @param button The RESET button.
- * @param cfg Pointer to the module config or NULL in global mode.
- *
- * This function is called when the user clicks on the RESET button. In global mode
- * (when cfg = NULL) the function will reset the settings to the efault values, while
- * in "local" mode (when cfg != NULL) the function will reset the module settings to
- * the global settings. This is done by removing the corresponding key from the GKeyFile.
- */
+/* Reset settings. */
 static void reset_cb(GtkWidget * button, gpointer cfg)
 {
     guint32         flags;
@@ -258,14 +115,93 @@ static void reset_cb(GtkWidget * button, gpointer cfg)
 static void toggle_cb(GtkToggleButton * toggle, gpointer data)
 {
     if (gtk_toggle_button_get_active(toggle))
-    {
         flags |= (1 << GPOINTER_TO_UINT(data));
-    }
     else
-    {
         flags &= ~(1 << GPOINTER_TO_UINT(data));
-    }
 
     /* clear dirty flag if we are back where we started */
     dirty = (flags != startflags);
+}
+
+static void create_reset_button(GKeyFile * cfg, GtkBox * vbox)
+{
+    GtkWidget      *button;
+    GtkWidget      *butbox;
+
+    button = gtk_button_new_with_label(_("Reset"));
+    g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(reset_cb), cfg);
+
+    if (cfg == NULL)
+    {
+        gtk_widget_set_tooltip_text(button,
+                                    _
+                                    ("Reset settings to the default values."));
+    }
+    else
+    {
+        gtk_widget_set_tooltip_text(button,
+                                    _
+                                    ("Reset module settings to the global values."));
+    }
+
+    butbox = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+    gtk_button_box_set_layout(GTK_BUTTON_BOX(butbox), GTK_BUTTONBOX_END);
+    gtk_box_pack_end(GTK_BOX(butbox), button, FALSE, TRUE, 10);
+    gtk_box_pack_end(vbox, butbox, FALSE, TRUE, 0);
+}
+
+GtkWidget      *sat_pref_list_view_create(GKeyFile * cfg)
+{
+    GtkWidget      *vbox;
+    gint            i;
+    GtkWidget      *table, *label;
+
+    /* column visibility selector */
+    if (cfg != NULL)
+    {
+        flags = mod_cfg_get_int(cfg,
+                                MOD_CFG_LIST_SECTION,
+                                MOD_CFG_LIST_COLUMNS,
+                                SAT_CFG_INT_LIST_COLUMNS);
+    }
+    else
+    {
+        flags = sat_cfg_get_int(SAT_CFG_INT_LIST_COLUMNS);
+    }
+
+    /* create the table */
+    table = gtk_grid_new();
+    gtk_grid_set_row_spacing(GTK_GRID(table), 10);
+    gtk_grid_set_column_spacing(GTK_GRID(table), 5);
+
+    /* create header */
+    label = gtk_label_new(NULL);
+    g_object_set(label, "xalign", 0.0, "yalign", 0.5, NULL);
+    gtk_label_set_markup(GTK_LABEL(label), _("<b>Visible Fields:</b>"));
+    gtk_grid_attach(GTK_GRID(table), label, 0, 0, 2, 1);
+
+    /* add fields except the last one (bold) */
+    for (i = 0; i < SAT_LIST_COL_NUMBER - 1; i++)
+    {
+        check[i] = gtk_check_button_new_with_label(_(SAT_LIST_COL_TITLE[i]));
+        gtk_widget_set_tooltip_text(check[i], _(SAT_LIST_COL_HINT[i]));
+        gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(check[i]),
+                                     flags & (1 << i));
+        g_signal_connect(check[i], "toggled",
+                         G_CALLBACK(toggle_cb), GUINT_TO_POINTER(i));
+        gtk_grid_attach(GTK_GRID(table), check[i],
+                        i % COLUMNS, Y0 + i / COLUMNS, 1, 1);
+    }
+
+    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_box_set_homogeneous(GTK_BOX(vbox), FALSE);
+    gtk_container_set_border_width(GTK_CONTAINER(vbox), 10);
+    gtk_box_pack_start(GTK_BOX(vbox), table, TRUE, TRUE, 0);
+    create_reset_button(cfg, GTK_BOX(vbox));
+
+    startflags = flags;
+    dirty = FALSE;
+    reset = FALSE;
+
+    return vbox;
 }
