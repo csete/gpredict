3622709746f96c75df5011cdb470691db6a87730
Phil Ashby <phil.github@ashbysoft.com>
2017-12-24 17:57:00 +0000

    C-89 compatible variables

--- win32/win32-fetch.c
+++ win32/win32-fetch.c
@@ -1,5 +1,32 @@
-// Fetch URL content to a specified handle using Wininet as no libcurl
-// dev libs available for Win32/64.
+/*
+    Gpredict: Real-time satellite tracking and orbit prediction program
+
+    Copyright (C)  2001-2017  Alexandru Csete, OZ9AEC.
+    Copyright (C)  2017 Phil Ashby, 2E0IPX.
+
+    Comments, questions and bugreports should be submitted via
+    http://sourceforge.net/projects/gpredict/
+    More details can be found at the project home page:
+
+            http://gpredict.oz9aec.net/
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, visit http://www.fsf.org/
+
+
+    Fetch URL content to a specified handle using Wininet as no libcurl
+    dev libs available for Win32/64.
+*/
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <wininet.h>
@@ -8,7 +35,10 @@
 
 int win32_fetch(char *url, FILE *file, char *proxy, char *ua)
 {
-    HINTERNET hInt;
+    HINTERNET hInt, hUrl;
+    char buf[8192];
+    DWORD nRead;
+
     if (proxy != NULL)
         hInt = InternetOpen(ua, INTERNET_OPEN_TYPE_PROXY, proxy, "<local>", 0);
     else
@@ -16,15 +46,13 @@ int win32_fetch(char *url, FILE *file, char *proxy, char *ua)
     if (NULL == hInt)
         return 0x80000000 | (int)GetLastError();
 
-    HINTERNET hUrl = InternetOpenUrl(hInt, url, NULL, 0, INTERNET_FLAG_RELOAD, 0);
+    hUrl = InternetOpenUrl(hInt, url, NULL, 0, INTERNET_FLAG_RELOAD, 0);
     if (NULL == hUrl)
     {
         InternetCloseHandle(hInt);
         return 0x40000000 | (int)GetLastError();
     }
 
-    char buf[8192];
-    DWORD nRead;
     while (InternetReadFile(hUrl, buf, 8192, &nRead))
     {
         if (nRead > 0)
