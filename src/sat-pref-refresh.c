1193e428c65b356bdcc6d848dc0c20aa20985b28
Alexandru Csete <oz9aec@gmail.com>
2017-12-24 20:03:06 +0100

    Migrate sat-pref-refresh to Gtk+ 3
    Issue #80.

--- src/sat-pref-refresh.c
+++ src/sat-pref-refresh.c
@@ -1,16 +1,8 @@
 /*
     Gpredict: Real-time satellite tracking and orbit prediction program
 
-    Copyright (C)  2001-2009  Alexandru Csete, OZ9AEC.
+    Copyright (C)  2001-2017  Alexandru Csete, OZ9AEC.
 
-    Authors: Alexandru Csete <oz9aec@gmail.com>
-
-    Comments, questions and bugreports should be submitted via
-    http://sourceforge.net/projects/gpredict/
-    More details can be found at the project home page:
-
-            http://gpredict.oz9aec.net/
- 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
@@ -45,213 +37,8 @@ static GtkWidget *singlespin;   /* spin button for single-sat view */
 static gboolean dirty = FALSE;  /* used to check whether any changes have occurred */
 static gboolean reset = FALSE;
 
-static void     spin_changed_cb(GtkWidget * spinner, gpointer data);
-static void     create_reset_button(GKeyFile * cfg, GtkBox * vbox);
-static void     reset_cb(GtkWidget * button, gpointer cfg);
 
-/**
- * Create and initialise widgets for the refresh rates tab.
- *
- * The widgets must be preloaded with values from config. If a config value
- * is NULL, sensible default values, eg. those from defaults.h should
- * be laoded.
- */
-GtkWidget      *sat_pref_refresh_create(GKeyFile * cfg)
-{
-    GtkWidget      *table;
-    GtkWidget      *vbox;
-    GtkWidget      *label;
-    gint            val;
-
-    dirty = FALSE;
-    reset = FALSE;
-
-    /* create table */
-    table = gtk_table_new(6, 3, FALSE);
-    gtk_table_set_row_spacings(GTK_TABLE(table), 10);
-    gtk_table_set_col_spacings(GTK_TABLE(table), 5);
-
-    /* data refresh */
-    label = gtk_label_new(_("Refresh data every"));
-    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), label, 0, 1, 0, 1,
-                     GTK_FILL, GTK_SHRINK, 0, 0);
-
-    dataspin = gtk_spin_button_new_with_range(100, 10000, 1);
-    gtk_spin_button_set_increments(GTK_SPIN_BUTTON(dataspin), 1, 100);
-    gtk_spin_button_set_numeric(GTK_SPIN_BUTTON(dataspin), TRUE);
-    gtk_spin_button_set_update_policy(GTK_SPIN_BUTTON(dataspin),
-                                      GTK_UPDATE_IF_VALID);
-    if (cfg != NULL)
-    {
-        val = mod_cfg_get_int(cfg,
-                              MOD_CFG_GLOBAL_SECTION,
-                              MOD_CFG_TIMEOUT_KEY, SAT_CFG_INT_MODULE_TIMEOUT);
-    }
-    else
-    {
-        val = sat_cfg_get_int(SAT_CFG_INT_MODULE_TIMEOUT);
-    }
-    gtk_spin_button_set_value(GTK_SPIN_BUTTON(dataspin), val);
-    g_signal_connect(G_OBJECT(dataspin), "value-changed",
-                     G_CALLBACK(spin_changed_cb), NULL);
-    gtk_table_attach(GTK_TABLE(table), dataspin, 1, 2, 0, 1,
-                     GTK_FILL, GTK_SHRINK, 0, 0);
-
-    label = gtk_label_new(_("[msec]"));
-    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), label, 2, 3, 0, 1,
-                     GTK_FILL | GTK_EXPAND, GTK_SHRINK, 0, 0);
-
-    /* separator */
-    gtk_table_attach(GTK_TABLE(table),
-                     gtk_hseparator_new(),
-                     0, 3, 1, 2, GTK_FILL | GTK_EXPAND, GTK_SHRINK, 0, 0);
-
-
-    /* List View */
-    label = gtk_label_new(_("Refresh list view every"));
-    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), label, 0, 1, 2, 3,
-                     GTK_FILL, GTK_SHRINK, 0, 0);
-
-    listspin = gtk_spin_button_new_with_range(1, 50, 1);
-    gtk_spin_button_set_increments(GTK_SPIN_BUTTON(listspin), 1, 5);
-    gtk_spin_button_set_numeric(GTK_SPIN_BUTTON(listspin), TRUE);
-    gtk_spin_button_set_update_policy(GTK_SPIN_BUTTON(listspin),
-                                      GTK_UPDATE_IF_VALID);
-    if (cfg != NULL)
-    {
-        val = mod_cfg_get_int(cfg,
-                              MOD_CFG_LIST_SECTION,
-                              MOD_CFG_LIST_REFRESH, SAT_CFG_INT_LIST_REFRESH);
-    }
-    else
-    {
-        val = sat_cfg_get_int(SAT_CFG_INT_LIST_REFRESH);
-    }
-    gtk_spin_button_set_value(GTK_SPIN_BUTTON(listspin), val);
-    g_signal_connect(G_OBJECT(listspin), "value-changed",
-                     G_CALLBACK(spin_changed_cb), NULL);
-    gtk_table_attach(GTK_TABLE(table), listspin, 1, 2, 2, 3,
-                     GTK_FILL, GTK_SHRINK, 0, 0);
-
-    label = gtk_label_new(_("[cycle]"));
-    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), label, 2, 3, 2, 3,
-                     GTK_FILL | GTK_EXPAND, GTK_SHRINK, 0, 0);
-
-    /* Map View */
-    label = gtk_label_new(_("Refresh map view every"));
-    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), label, 0, 1, 3, 4,
-                     GTK_FILL, GTK_SHRINK, 0, 0);
-
-    mapspin = gtk_spin_button_new_with_range(1, 50, 1);
-    gtk_spin_button_set_increments(GTK_SPIN_BUTTON(mapspin), 1, 5);
-    gtk_spin_button_set_numeric(GTK_SPIN_BUTTON(mapspin), TRUE);
-    gtk_spin_button_set_update_policy(GTK_SPIN_BUTTON(mapspin),
-                                      GTK_UPDATE_IF_VALID);
-    if (cfg != NULL)
-    {
-        val = mod_cfg_get_int(cfg,
-                              MOD_CFG_MAP_SECTION,
-                              MOD_CFG_MAP_REFRESH, SAT_CFG_INT_MAP_REFRESH);
-    }
-    else
-    {
-        val = sat_cfg_get_int(SAT_CFG_INT_MAP_REFRESH);
-    }
-    gtk_spin_button_set_value(GTK_SPIN_BUTTON(mapspin), val);
-    g_signal_connect(G_OBJECT(mapspin), "value-changed",
-                     G_CALLBACK(spin_changed_cb), NULL);
-    gtk_table_attach(GTK_TABLE(table), mapspin, 1, 2, 3, 4,
-                     GTK_FILL, GTK_SHRINK, 0, 0);
-
-    label = gtk_label_new(_("[cycle]"));
-    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), label, 2, 3, 3, 4,
-                     GTK_FILL | GTK_EXPAND, GTK_SHRINK, 0, 0);
-
-
-    /* Polar View */
-    label = gtk_label_new(_("Refresh polar view every"));
-    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), label, 0, 1, 4, 5,
-                     GTK_FILL, GTK_SHRINK, 0, 0);
-
-    polarspin = gtk_spin_button_new_with_range(1, 50, 1);
-    gtk_spin_button_set_increments(GTK_SPIN_BUTTON(polarspin), 1, 5);
-    gtk_spin_button_set_numeric(GTK_SPIN_BUTTON(polarspin), TRUE);
-    gtk_spin_button_set_update_policy(GTK_SPIN_BUTTON(polarspin),
-                                      GTK_UPDATE_IF_VALID);
-    if (cfg != NULL)
-    {
-        val = mod_cfg_get_int(cfg,
-                              MOD_CFG_POLAR_SECTION,
-                              MOD_CFG_POLAR_REFRESH,
-                              SAT_CFG_INT_POLAR_REFRESH);
-    }
-    else
-    {
-        val = sat_cfg_get_int(SAT_CFG_INT_POLAR_REFRESH);
-    }
-    gtk_spin_button_set_value(GTK_SPIN_BUTTON(polarspin), val);
-    g_signal_connect(G_OBJECT(polarspin), "value-changed",
-                     G_CALLBACK(spin_changed_cb), NULL);
-    gtk_table_attach(GTK_TABLE(table), polarspin, 1, 2, 4, 5,
-                     GTK_FILL, GTK_SHRINK, 0, 0);
-
-    label = gtk_label_new(_("[cycle]"));
-    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), label, 2, 3, 4, 5,
-                     GTK_FILL | GTK_EXPAND, GTK_SHRINK, 0, 0);
-
-    /* Single-Sat View */
-    label = gtk_label_new(_("Refresh single-sat view every"));
-    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), label, 0, 1, 5, 6,
-                     GTK_FILL, GTK_SHRINK, 0, 0);
-
-    singlespin = gtk_spin_button_new_with_range(1, 50, 1);
-    gtk_spin_button_set_increments(GTK_SPIN_BUTTON(singlespin), 1, 5);
-    gtk_spin_button_set_numeric(GTK_SPIN_BUTTON(singlespin), TRUE);
-    gtk_spin_button_set_update_policy(GTK_SPIN_BUTTON(singlespin),
-                                      GTK_UPDATE_IF_VALID);
-    if (cfg != NULL)
-    {
-        val = mod_cfg_get_int(cfg,
-                              MOD_CFG_SINGLE_SAT_SECTION,
-                              MOD_CFG_SINGLE_SAT_REFRESH,
-                              SAT_CFG_INT_SINGLE_SAT_REFRESH);
-    }
-    else
-    {
-        val = sat_cfg_get_int(SAT_CFG_INT_SINGLE_SAT_REFRESH);
-    }
-    gtk_spin_button_set_value(GTK_SPIN_BUTTON(singlespin), val);
-    g_signal_connect(G_OBJECT(singlespin), "value-changed",
-                     G_CALLBACK(spin_changed_cb), NULL);
-    gtk_table_attach(GTK_TABLE(table), singlespin, 1, 2, 5, 6,
-                     GTK_FILL, GTK_SHRINK, 0, 0);
-
-    label = gtk_label_new(_("[cycle]"));
-    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), label, 2, 3, 5, 6,
-                     GTK_FILL | GTK_EXPAND, GTK_SHRINK, 0, 0);
-
-    /* create vertical box */
-    vbox = gtk_vbox_new(FALSE, 0);
-    gtk_container_set_border_width(GTK_CONTAINER(vbox), 20);
-    gtk_box_pack_start(GTK_BOX(vbox), table, TRUE, TRUE, 0);
-
-    /* create RESET button */
-    create_reset_button(cfg, GTK_BOX(vbox));
-
-    return vbox;
-}
-
-/** User pressed cancel. Any changes to config must be cancelled. */
+/* User pressed cancel. Any changes to config must be cancelled. */
 void sat_pref_refresh_cancel(GKeyFile * cfg)
 {
     (void)cfg;
@@ -259,7 +46,7 @@ void sat_pref_refresh_cancel(GKeyFile * cfg)
     dirty = FALSE;
 }
 
-/** User pressed OK. Any changes should be stored in config. */
+/* User pressed OK. Any changes should be stored in config. */
 void sat_pref_refresh_ok(GKeyFile * cfg)
 {
     if (dirty)
@@ -365,43 +152,8 @@ static void spin_changed_cb(GtkWidget * spinner, gpointer data)
 }
 
 /**
- * Create RESET button.
- * @param cfg Config data or NULL in global mode.
- * @param vbox The container.
- *
- * This function creates and sets up the view selector combos.
- */
-static void create_reset_button(GKeyFile * cfg, GtkBox * vbox)
-{
-    GtkWidget      *button;
-    GtkWidget      *butbox;
-
-    button = gtk_button_new_with_label(_("Reset"));
-    g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(reset_cb), cfg);
-
-    if (cfg == NULL)
-    {
-        gtk_widget_set_tooltip_text(button,
-                                    _
-                                    ("Reset settings to the default values."));
-    }
-    else
-    {
-        gtk_widget_set_tooltip_text(button,
-                                    _
-                                    ("Reset module settings to the global values."));
-    }
-
-    butbox = gtk_hbutton_box_new();
-    gtk_button_box_set_layout(GTK_BUTTON_BOX(butbox), GTK_BUTTONBOX_END);
-    gtk_box_pack_end(GTK_BOX(butbox), button, FALSE, TRUE, 10);
-
-    gtk_box_pack_end(vbox, butbox, FALSE, TRUE, 0);
-
-}
-
-/**
  * Reset settings.
+ *
  * @param button The RESET button.
  * @param cfg Pointer to the module config or NULL in global mode.
  *
@@ -449,4 +201,209 @@ static void reset_cb(GtkWidget * button, gpointer cfg)
     /* reset flags */
     reset = TRUE;
     dirty = FALSE;
+}
+
+static void create_reset_button(GKeyFile * cfg, GtkBox * vbox)
+{
+    GtkWidget      *button;
+    GtkWidget      *butbox;
+
+    button = gtk_button_new_with_label(_("Reset"));
+    g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(reset_cb), cfg);
+
+    if (cfg == NULL)
+    {
+        gtk_widget_set_tooltip_text(button,
+                                    _
+                                    ("Reset settings to the default values."));
+    }
+    else
+    {
+        gtk_widget_set_tooltip_text(button,
+                                    _
+                                    ("Reset module settings to the global values."));
+    }
+
+    butbox = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+    gtk_button_box_set_layout(GTK_BUTTON_BOX(butbox), GTK_BUTTONBOX_END);
+    gtk_box_pack_end(GTK_BOX(butbox), button, FALSE, TRUE, 10);
+
+    gtk_box_pack_end(vbox, butbox, FALSE, TRUE, 0);
+
+}
+
+GtkWidget      *sat_pref_refresh_create(GKeyFile * cfg)
+{
+    GtkWidget      *table;
+    GtkWidget      *vbox;
+    GtkWidget      *label;
+    gint            val;
+
+    dirty = FALSE;
+    reset = FALSE;
+
+    table = gtk_grid_new();
+    gtk_grid_set_row_spacing(GTK_GRID(table), 10);
+    gtk_grid_set_column_spacing(GTK_GRID(table), 5);
+
+    /* data refresh */
+    label = gtk_label_new(_("Refresh data every"));
+    g_object_set(label, "xalign", 0.0, "yalign", 0.5, NULL);
+    gtk_grid_attach(GTK_GRID(table), label, 0, 0, 1, 1);
+
+    dataspin = gtk_spin_button_new_with_range(100, 10000, 1);
+    gtk_spin_button_set_increments(GTK_SPIN_BUTTON(dataspin), 1, 100);
+    gtk_spin_button_set_numeric(GTK_SPIN_BUTTON(dataspin), TRUE);
+    gtk_spin_button_set_update_policy(GTK_SPIN_BUTTON(dataspin),
+                                      GTK_UPDATE_IF_VALID);
+    if (cfg != NULL)
+    {
+        val = mod_cfg_get_int(cfg,
+                              MOD_CFG_GLOBAL_SECTION,
+                              MOD_CFG_TIMEOUT_KEY, SAT_CFG_INT_MODULE_TIMEOUT);
+    }
+    else
+    {
+        val = sat_cfg_get_int(SAT_CFG_INT_MODULE_TIMEOUT);
+    }
+    gtk_spin_button_set_value(GTK_SPIN_BUTTON(dataspin), val);
+    g_signal_connect(G_OBJECT(dataspin), "value-changed",
+                     G_CALLBACK(spin_changed_cb), NULL);
+    gtk_grid_attach(GTK_GRID(table), dataspin, 1, 0, 1, 1);
+
+    label = gtk_label_new(_("[msec]"));
+    g_object_set(label, "xalign", 0.0, "yalign", 0.5, NULL);
+    gtk_grid_attach(GTK_GRID(table), label, 2, 0, 1, 1);
+
+    gtk_grid_attach(GTK_GRID(table),
+                    gtk_separator_new(GTK_ORIENTATION_HORIZONTAL), 0, 1, 3, 1);
+
+    /* List View */
+    label = gtk_label_new(_("Refresh list view every"));
+    g_object_set(label, "xalign", 0.0, "yalign", 0.5, NULL);
+    gtk_grid_attach(GTK_GRID(table), label, 0, 2, 1, 1);
+
+    listspin = gtk_spin_button_new_with_range(1, 50, 1);
+    gtk_spin_button_set_increments(GTK_SPIN_BUTTON(listspin), 1, 5);
+    gtk_spin_button_set_numeric(GTK_SPIN_BUTTON(listspin), TRUE);
+    gtk_spin_button_set_update_policy(GTK_SPIN_BUTTON(listspin),
+                                      GTK_UPDATE_IF_VALID);
+    if (cfg != NULL)
+    {
+        val = mod_cfg_get_int(cfg,
+                              MOD_CFG_LIST_SECTION,
+                              MOD_CFG_LIST_REFRESH, SAT_CFG_INT_LIST_REFRESH);
+    }
+    else
+    {
+        val = sat_cfg_get_int(SAT_CFG_INT_LIST_REFRESH);
+    }
+    gtk_spin_button_set_value(GTK_SPIN_BUTTON(listspin), val);
+    g_signal_connect(G_OBJECT(listspin), "value-changed",
+                     G_CALLBACK(spin_changed_cb), NULL);
+    gtk_grid_attach(GTK_GRID(table), listspin, 1, 2, 1, 1);
+
+    label = gtk_label_new(_("[cycle]"));
+    g_object_set(label, "xalign", 0.0, "yalign", 0.5, NULL);
+    gtk_grid_attach(GTK_GRID(table), label, 2, 2, 1, 1);
+
+    /* Map View */
+    label = gtk_label_new(_("Refresh map view every"));
+    g_object_set(label, "xalign", 0.0, "yalign", 0.5, NULL);
+    gtk_grid_attach(GTK_GRID(table), label, 0, 3, 1, 1);
+
+    mapspin = gtk_spin_button_new_with_range(1, 50, 1);
+    gtk_spin_button_set_increments(GTK_SPIN_BUTTON(mapspin), 1, 5);
+    gtk_spin_button_set_numeric(GTK_SPIN_BUTTON(mapspin), TRUE);
+    gtk_spin_button_set_update_policy(GTK_SPIN_BUTTON(mapspin),
+                                      GTK_UPDATE_IF_VALID);
+    if (cfg != NULL)
+    {
+        val = mod_cfg_get_int(cfg,
+                              MOD_CFG_MAP_SECTION,
+                              MOD_CFG_MAP_REFRESH, SAT_CFG_INT_MAP_REFRESH);
+    }
+    else
+    {
+        val = sat_cfg_get_int(SAT_CFG_INT_MAP_REFRESH);
+    }
+    gtk_spin_button_set_value(GTK_SPIN_BUTTON(mapspin), val);
+    g_signal_connect(G_OBJECT(mapspin), "value-changed",
+                     G_CALLBACK(spin_changed_cb), NULL);
+    gtk_grid_attach(GTK_GRID(table), mapspin, 1, 3, 1, 1);
+
+    label = gtk_label_new(_("[cycle]"));
+    g_object_set(label, "xalign", 0.0, "yalign", 0.5, NULL);
+    gtk_grid_attach(GTK_GRID(table), label, 2, 3, 1, 1);
+
+    /* Polar View */
+    label = gtk_label_new(_("Refresh polar view every"));
+    g_object_set(label, "xalign", 0.0, "yalign", 0.5, NULL);
+    gtk_grid_attach(GTK_GRID(table), label, 0, 4, 1, 1);
+
+    polarspin = gtk_spin_button_new_with_range(1, 50, 1);
+    gtk_spin_button_set_increments(GTK_SPIN_BUTTON(polarspin), 1, 5);
+    gtk_spin_button_set_numeric(GTK_SPIN_BUTTON(polarspin), TRUE);
+    gtk_spin_button_set_update_policy(GTK_SPIN_BUTTON(polarspin),
+                                      GTK_UPDATE_IF_VALID);
+    if (cfg != NULL)
+    {
+        val = mod_cfg_get_int(cfg,
+                              MOD_CFG_POLAR_SECTION,
+                              MOD_CFG_POLAR_REFRESH,
+                              SAT_CFG_INT_POLAR_REFRESH);
+    }
+    else
+    {
+        val = sat_cfg_get_int(SAT_CFG_INT_POLAR_REFRESH);
+    }
+    gtk_spin_button_set_value(GTK_SPIN_BUTTON(polarspin), val);
+    g_signal_connect(G_OBJECT(polarspin), "value-changed",
+                     G_CALLBACK(spin_changed_cb), NULL);
+    gtk_grid_attach(GTK_GRID(table), polarspin, 1, 4, 1, 1);
+
+    label = gtk_label_new(_("[cycle]"));
+    g_object_set(label, "xalign", 0.0, "yalign", 0.5, NULL);
+    gtk_grid_attach(GTK_GRID(table), label, 2, 4, 1, 1);
+
+    /* Single-Sat View */
+    label = gtk_label_new(_("Refresh single-sat view every"));
+    g_object_set(label, "xalign", 0.0, "yalign", 0.5, NULL);
+    gtk_grid_attach(GTK_GRID(table), label, 0, 5, 1, 1);
+
+    singlespin = gtk_spin_button_new_with_range(1, 50, 1);
+    gtk_spin_button_set_increments(GTK_SPIN_BUTTON(singlespin), 1, 5);
+    gtk_spin_button_set_numeric(GTK_SPIN_BUTTON(singlespin), TRUE);
+    gtk_spin_button_set_update_policy(GTK_SPIN_BUTTON(singlespin),
+                                      GTK_UPDATE_IF_VALID);
+    if (cfg != NULL)
+    {
+        val = mod_cfg_get_int(cfg,
+                              MOD_CFG_SINGLE_SAT_SECTION,
+                              MOD_CFG_SINGLE_SAT_REFRESH,
+                              SAT_CFG_INT_SINGLE_SAT_REFRESH);
+    }
+    else
+    {
+        val = sat_cfg_get_int(SAT_CFG_INT_SINGLE_SAT_REFRESH);
+    }
+    gtk_spin_button_set_value(GTK_SPIN_BUTTON(singlespin), val);
+    g_signal_connect(G_OBJECT(singlespin), "value-changed",
+                     G_CALLBACK(spin_changed_cb), NULL);
+    gtk_grid_attach(GTK_GRID(table), singlespin, 1, 5, 1, 1);
+
+    label = gtk_label_new(_("[cycle]"));
+    g_object_set(label, "xalign", 0.0, "yalign", 0.5, NULL);
+    gtk_grid_attach(GTK_GRID(table), label, 2, 5, 1, 1);
+
+    /* create vertical box */
+    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_box_set_homogeneous(GTK_BOX(vbox), FALSE);
+    gtk_container_set_border_width(GTK_CONTAINER(vbox), 20);
+    gtk_box_pack_start(GTK_BOX(vbox), table, TRUE, TRUE, 0);
+
+    /* create RESET button */
+    create_reset_button(cfg, GTK_BOX(vbox));
+
+    return vbox;
 }
