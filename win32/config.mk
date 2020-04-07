aa0afb5cf162c24eaecfe25fd9b5d147fe660a77
Alan Moffet <amoffet@hotmail.com>
2020-04-07 10:22:07 -0600

    Fix Window build

--- win32/config.mk
+++ win32/config.mk
@@ -12,15 +12,34 @@
 # (thanks GNOME for missing crucial bits out of builds). My versions
 # are in this folder, and will need unpack paths inserting.
 
-MINGW_ROOT=../../mingw32
-#MINGW_ROOT=../../../tmp/mingw32
+# Change the path below to select the appropriate install path and compiler 
+# for 32 and 64 bit cross-compiles.
+#
+# Windows hosts will require a configuration like this for 32 bit compiles:
+# MINGW_ROOT=/mingw32
+# or this, for 64 bit compiles:
+# MINGW_ROOT=/mingw64
+#
+# Linux or other hosts require a path that is dependent upon the installation.
+# For 32 bit:
+# MINGW_ROOT=../../msys64/mingw32
+# For 64 bit:
+# MINGW_ROOT=../../msys64/mingw64
+#
+#MINGW_ROOT=/mingw32
+#MGW_PREFIX=i686-w64-mingw32-
+
+MINGW_ROOT=/mingw32
+#MGW_PREFIX=x86_64-w64-mingw32-
 
 PKG_CONFIG_PATH = "$(abspath $(MINGW_ROOT)/lib/pkgconfig)"
 
 # binary dependencies to be deployed with gpredict.exe
 BINDEPS = \
 	$(wildcard $(MINGW_ROOT)/bin/*.dll) \
-	$(MINGW_ROOT)/lib/gdk-pixbuf-2.0/2.10.0/loaders/libpixbufloader-svg.dll
+	$(MINGW_ROOT)/lib/gdk-pixbuf-2.0/2.10.0/loaders/libpixbufloader-svg.dll \
+	$(MINGW_ROOT)/lib/gdk-pixbuf-2.0/2.10.0/loaders/libpixbufloader-png.dll \
+	$(MINGW_ROOT)/lib/gdk-pixbuf-2.0/2.10.0/loaders/libpixbufloader-jpeg.dll
 
 # where to put the loaders.cache file
 LOADERS = lib/gdk-pixbuf-2.0/2.10.0
@@ -28,6 +47,7 @@ LOADERS = lib/gdk-pixbuf-2.0/2.10.0
 # other miscellaneous folders to deploy with the binary
 GTKETC  = $(MINGW_ROOT)/etc
 SCHEMAS = $(MINGW_ROOT)/share/glib-2.0/schemas
+ADWAITA = $(MINGW_ROOT)/share/icons/Adwaita
 
 # Autoversioning from nearest git tag, assumes v<x>.<y> tag format.
 
