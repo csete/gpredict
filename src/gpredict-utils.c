327b254eec0b2dd390dab98a5a7198dcdce69876
Alexandru Csete <oz9aec@gmail.com>
2026-08-15 21:24:11 +0200

    Format GpredictUtils

--- src/gpredict-utils.c
+++ src/gpredict-utils.c
@@ -2,17 +2,17 @@
   Gpredict: Real-time satellite tracking and orbit prediction program
 
   Copyright (C)  2001-2019  Alexandru Csete, OZ9AEC
- 
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program; if not, visit http://www.fsf.org/
 */
@@ -30,21 +30,19 @@
 #include "sat-log.h"
 #include "strnatcmp.h"
 
-
-
 /**
  * Create a horizontal pixmap button.
  *
  * The text will be placed to the right of the image.
  * file is only the icon name, not the full path.
  */
-GtkWidget      *gpredict_hpixmap_button(const gchar * file, const gchar * text,
-                                        const gchar * tooltip)
+GtkWidget *gpredict_hpixmap_button(const gchar *file, const gchar *text,
+                                   const gchar *tooltip)
 {
-    GtkWidget      *button;
-    GtkWidget      *image;
-    GtkWidget      *box;
-    gchar          *path;
+    GtkWidget *button;
+    GtkWidget *image;
+    GtkWidget *box;
+    gchar *path;
 
     path = icon_file_name(file);
     image = gtk_image_new_from_file(path);
@@ -68,13 +66,13 @@ GtkWidget      *gpredict_hpixmap_button(const gchar * file, const gchar * text,
  * The text will be placed under the image.
  * file is only the icon name, not the full path.
  */
-GtkWidget      *gpredict_vpixmap_button(const gchar * file, const gchar * text,
-                                        const gchar * tooltip)
+GtkWidget *gpredict_vpixmap_button(const gchar *file, const gchar *text,
+                                   const gchar *tooltip)
 {
-    GtkWidget      *button;
-    GtkWidget      *image;
-    GtkWidget      *box;
-    gchar          *path;
+    GtkWidget *button;
+    GtkWidget *image;
+    GtkWidget *box;
+    gchar *path;
 
     path = icon_file_name(file);
     image = gtk_image_new_from_file(path);
@@ -98,13 +96,12 @@ GtkWidget      *gpredict_vpixmap_button(const gchar * file, const gchar * text,
  * The text will be placed to the right of the image.
  * The icon size will be GTK_ICON_SIZE_BUTTON.
  */
-GtkWidget      *gpredict_hstock_button(const gchar * stock_id,
-                                       const gchar * text,
-                                       const gchar * tooltip)
+GtkWidget *gpredict_hstock_button(const gchar *stock_id, const gchar *text,
+                                  const gchar *tooltip)
 {
-    GtkWidget      *button;
-    GtkWidget      *image;
-    GtkWidget      *box;
+    GtkWidget *button;
+    GtkWidget *image;
+    GtkWidget *box;
 
     image = gtk_image_new_from_icon_name(stock_id, GTK_ICON_SIZE_BUTTON);
     box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
@@ -129,7 +126,7 @@ GtkWidget      *gpredict_hstock_button(const gchar * stock_id,
  * This function is called by the \a grig_set_combo_tooltips function which
  * is must be used as callback for the "realize" signal of the GtkComboBox.
  */
-static void set_combo_tooltip(GtkWidget * combo, gpointer text)
+static void set_combo_tooltip(GtkWidget *combo, gpointer text)
 {
     /* if current child is a button we have BINGO! */
     if (GTK_IS_BUTTON(combo))
@@ -152,26 +149,25 @@ static void set_combo_tooltip(GtkWidget * combo, gpointer text)
  * @note This works only if the function is actually used as callback for the
  *       @a realize signal og the GtkComboBox.
  *
- * @note This great trick has been pointed out by Matthias Clasen, he has done the
- *       the same for the filter combo in the new GtkFileChooser
- *       ref: gtkfilechooserdefault.c:3151 in Gtk+ 2.5.5
+ * @note This great trick has been pointed out by Matthias Clasen, he has done
+ * the the same for the filter combo in the new GtkFileChooser ref:
+ * gtkfilechooserdefault.c:3151 in Gtk+ 2.5.5
  */
-void gpredict_set_combo_tooltips(GtkWidget * combo, gpointer text)
+void gpredict_set_combo_tooltips(GtkWidget *combo, gpointer text)
 {
     /* for each child in the container call the internal
        function which actually creates the tooltips.
      */
     gtk_container_forall(GTK_CONTAINER(combo), set_combo_tooltip, text);
-
 }
 
-gint gpredict_file_copy(const gchar * in, const gchar * out)
+gint gpredict_file_copy(const gchar *in, const gchar *out)
 {
-    gchar          *contents;
-    gboolean        status = 0;
-    GError         *err = NULL;
-    gsize           ulen;
-    gssize          slen;
+    gchar *contents;
+    gboolean status = 0;
+    GError *err = NULL;
+    gsize ulen;
+    gssize slen;
 
     g_return_val_if_fail(in != NULL, 1);
     g_return_val_if_fail(out != NULL, 1);
@@ -187,7 +183,7 @@ gint gpredict_file_copy(const gchar * in, const gchar * out)
     else
     {
         /* write contents to new file */
-        slen = (gssize) ulen;
+        slen = (gssize)ulen;
 
         if (!g_file_set_contents(out, contents, slen, &err))
         {
@@ -208,12 +204,12 @@ gint gpredict_file_copy(const gchar * in, const gchar * out)
  *
  * Pixmapfile is only the icon name, not the full path.
  */
-GtkWidget      *gpredict_mini_mod_button(const gchar * pixmapfile,
-                                         const gchar * tooltip)
+GtkWidget *gpredict_mini_mod_button(const gchar *pixmapfile,
+                                    const gchar *tooltip)
 {
-    GtkWidget      *button;
-    GtkWidget      *image;
-    gchar          *path;
+    GtkWidget *button;
+    GtkWidget *image;
+    gchar *path;
 
     path = icon_file_name(pixmapfile);
     image = gtk_image_new_from_file(path);
@@ -229,35 +225,35 @@ GtkWidget      *gpredict_mini_mod_button(const gchar * pixmapfile,
 
 /**
  * Convert a BCD colour to a GdkColor structure.
- * 
+ *
  * @param rgb The source colour in 0xRRGGBB form.
  * @param color Pointer to an existing GdkColor structure.
  */
-void rgb2gdk(guint rgb, GdkColor * color)
+void rgb2gdk(guint rgb, GdkColor *color)
 {
-    guint16         r, g, b;
-    guint           tmp;
+    guint16 r, g, b;
+    guint tmp;
 
     /* sanity checks */
     if (color == NULL)
     {
         sat_log_log(SAT_LOG_LEVEL_ERROR,
-                    _("%s:%s: %s called with color = NULL"),
-                    __FILE__, __LINE__, __func__);
+                    _("%s:%s: %s called with color = NULL"), __FILE__, __LINE__,
+                    __func__);
         return;
     }
 
     /* red */
     tmp = rgb & 0xFF0000;
-    r = (guint16) (tmp >> 16);
+    r = (guint16)(tmp >> 16);
 
     /* green */
     tmp = rgb & 0x00FF00;
-    g = (guint16) (tmp >> 8);
+    g = (guint16)(tmp >> 8);
 
     /* blue */
     tmp = rgb & 0x0000FF;
-    b = (guint16) tmp;
+    b = (guint16)tmp;
 
     /* store colours */
     color->red = 257 * r;
@@ -272,41 +268,41 @@ void rgb2gdk(guint rgb, GdkColor * color)
  * @param color Pointer to an existing GdkColor structure.
  * @param alpha Pointer to where the alpha channel value should be stored
  */
-void rgba2gdk(guint rgba, GdkColor * color, guint16 * alpha)
+void rgba2gdk(guint rgba, GdkColor *color, guint16 *alpha)
 {
-    guint16         r, g, b;
-    guint           tmp;
+    guint16 r, g, b;
+    guint tmp;
 
     /* sanity checks */
     if (color == NULL)
     {
         sat_log_log(SAT_LOG_LEVEL_ERROR,
-                    _("%s:%s: %s called with color = NULL"),
-                    __FILE__, __LINE__, __func__);
+                    _("%s:%s: %s called with color = NULL"), __FILE__, __LINE__,
+                    __func__);
         return;
     }
     if (alpha == NULL)
     {
         sat_log_log(SAT_LOG_LEVEL_ERROR,
-                    _("%s:%s: %s called with alpha = NULL"),
-                    __FILE__, __LINE__, __func__);
+                    _("%s:%s: %s called with alpha = NULL"), __FILE__, __LINE__,
+                    __func__);
         return;
     }
 
     /* red */
     tmp = rgba & 0xFF000000;
-    r = (guint16) (tmp >> 24);
+    r = (guint16)(tmp >> 24);
 
     /* green */
     tmp = rgba & 0x00FF0000;
-    g = (guint16) (tmp >> 16);
+    g = (guint16)(tmp >> 16);
 
     /* blue */
     tmp = rgba & 0x0000FF00;
-    b = (guint16) (tmp >> 8);
+    b = (guint16)(tmp >> 8);
 
     /* alpha channel */
-    *alpha = (guint16) (257 * (rgba & 0x000000FF));
+    *alpha = (guint16)(257 * (rgba & 0x000000FF));
 
     /* store colours */
     color->red = 257 * r;
@@ -320,38 +316,37 @@ void rgba2gdk(guint rgba, GdkColor * color, guint16 * alpha)
  * @param color The GdkColor structure.
  * @param rgb Pointer to where the 0xRRGGBB encoded colour should be stored.
  */
-void gdk2rgb(const GdkColor * color, guint * rgb)
+void gdk2rgb(const GdkColor *color, guint *rgb)
 {
-    guint           r, g, b;
-    guint16         tmp;
+    guint r, g, b;
+    guint16 tmp;
 
     /* sanity checks */
     if (color == NULL)
     {
         sat_log_log(SAT_LOG_LEVEL_ERROR,
-                    _("%s:%s: %s called with color = NULL"),
-                    __FILE__, __LINE__, __func__);
+                    _("%s:%s: %s called with color = NULL"), __FILE__, __LINE__,
+                    __func__);
         return;
     }
     if (rgb == NULL)
     {
-        sat_log_log(SAT_LOG_LEVEL_ERROR,
-                    _("%s:%s: %s called with rgb = NULL"),
+        sat_log_log(SAT_LOG_LEVEL_ERROR, _("%s:%s: %s called with rgb = NULL"),
                     __FILE__, __LINE__, __func__);
         return;
     }
 
     /* red */
     tmp = color->red / 257;
-    r = (guint) (tmp << 16);
+    r = (guint)(tmp << 16);
 
     /* green */
     tmp = color->green / 257;
-    g = (guint) (tmp << 8);
+    g = (guint)(tmp << 8);
 
     /* blue */
     tmp = color->blue / 257;
-    b = (guint) tmp;
+    b = (guint)tmp;
 
     *rgb = (r | g | b);
 }
@@ -363,42 +358,41 @@ void gdk2rgb(const GdkColor * color, guint * rgb)
  * @param alpha The value of the alpha channel.
  * @param rgb Pointer to where the 0xRRGGBBAA encoded colour should be stored.
  */
-void gdk2rgba(const GdkColor * color, guint16 alpha, guint * rgba)
+void gdk2rgba(const GdkColor *color, guint16 alpha, guint *rgba)
 {
-    guint           r, g, b, a;
-    guint16         tmp;
+    guint r, g, b, a;
+    guint16 tmp;
 
     /* sanity checks */
     if (color == NULL)
     {
         sat_log_log(SAT_LOG_LEVEL_ERROR,
-                    _("%s:%s: %s called with color = NULL"),
-                    __FILE__, __LINE__, __func__);
+                    _("%s:%s: %s called with color = NULL"), __FILE__, __LINE__,
+                    __func__);
         return;
     }
     if (rgba == NULL)
     {
-        sat_log_log(SAT_LOG_LEVEL_ERROR,
-                    _("%s:%s: %s called with rgba = NULL"),
+        sat_log_log(SAT_LOG_LEVEL_ERROR, _("%s:%s: %s called with rgba = NULL"),
                     __FILE__, __LINE__, __func__);
         return;
     }
 
     /* red */
     tmp = color->red / 257;
-    r = (guint) (tmp << 24);
+    r = (guint)(tmp << 24);
 
     /* green */
     tmp = color->green / 257;
-    g = (guint) (tmp << 16);
+    g = (guint)(tmp << 16);
 
     /* blue */
     tmp = color->blue / 257;
-    b = (guint) (tmp << 8);
+    b = (guint)(tmp << 8);
 
     /* alpha */
     tmp = alpha / 257;
-    a = (guint) tmp;
+    a = (guint)tmp;
 
     *rgba = (r | g | b | a);
 }
@@ -409,22 +403,22 @@ void gdk2rgba(const GdkColor * color, guint16 alpha, guint * rgba)
  * @param color The GdkColor structure.
  * @return A newly allocated character string.
  */
-gchar          *rgba2html(guint rgba)
+gchar *rgba2html(guint rgba)
 {
-    gchar          *col;
-    guint8          r, g, b;
-    guint           tmp;
+    gchar *col;
+    guint8 r, g, b;
+    guint tmp;
 
     tmp = rgba & 0xFF000000;
-    r = (guint8) (tmp >> 24);
+    r = (guint8)(tmp >> 24);
 
     /* green */
     tmp = rgba & 0x00FF0000;
-    g = (guint8) (tmp >> 16);
+    g = (guint8)(tmp >> 16);
 
     /* blue */
     tmp = rgba & 0x0000FF00;
-    b = (guint8) (tmp >> 8);
+    b = (guint8)(tmp >> 8);
 
     col = g_strdup_printf("%X%X%X", r, g, b);
 
@@ -460,13 +454,14 @@ int gpredict_strcmp(const char *s1, const char *s2)
  * @param s1 the larger string
  * @param s2 the substring that we are searching for.
  * @return pointer to the substring location
- *  
- *  this is a substitute for strcasestr which is a gnu c extension and not available everywhere.
+ *
+ *  this is a substitute for strcasestr which is a gnu c extension and not
+ * available everywhere.
  */
-char           *gpredict_strcasestr(const char *s1, const char *s2)
+char *gpredict_strcasestr(const char *s1, const char *s2)
 {
-    size_t          s1_len = strlen(s1);
-    size_t          s2_len = strlen(s2);
+    size_t s1_len = strlen(s1);
+    size_t s2_len = strlen(s2);
 
     while (s1_len >= s2_len)
     {
@@ -488,15 +483,14 @@ char           *gpredict_strcasestr(const char *s1, const char *s2)
  * @return 1 on error and zero on success.
  *
  */
-gboolean gpredict_save_key_file(GKeyFile * cfgdata, const char *filename)
+gboolean gpredict_save_key_file(GKeyFile *cfgdata, const char *filename)
 {
-    GError         *error = NULL; 
+    GError *error = NULL;
 
     if (!g_key_file_save_to_file(cfgdata, filename, &error))
     {
         sat_log_log(SAT_LOG_LEVEL_ERROR,
-                    _("%s: Error writing config data (%s)."),
-                    __func__,
+                    _("%s: Error writing config data (%s)."), __func__,
                     error != NULL ? error->message : "unknown error");
         g_clear_error(&error);
         return 1;
@@ -504,7 +498,6 @@ gboolean gpredict_save_key_file(GKeyFile * cfgdata, const char *filename)
 
     return 0;
 }
-
 
 /**
  * Check if \c ch is an alpha-num; in range \c "[0-9a-zA-F]".
@@ -520,14 +513,13 @@ gboolean gpredict_legal_char(int ch)
     return (FALSE);
 }
 
-
 /* Convert a 0xRRGGBBAA encoded config integer to a GdkRGBA structure */
-void rgba_from_cfg(guint cfg_rgba, GdkRGBA * gdk_rgba)
+void rgba_from_cfg(guint cfg_rgba, GdkRGBA *gdk_rgba)
 {
     if (gdk_rgba == NULL)
     {
-        sat_log_log(SAT_LOG_LEVEL_ERROR,
-                    _("%s called with gdk_rgba = NULL"), __func__);
+        sat_log_log(SAT_LOG_LEVEL_ERROR, _("%s called with gdk_rgba = NULL"),
+                    __func__);
         return;
     }
 
@@ -538,32 +530,33 @@ void rgba_from_cfg(guint cfg_rgba, GdkRGBA * gdk_rgba)
 }
 
 /* convert GdkRGBA struct to 0xRRGGBBAA formatted config integer */
-guint rgba_to_cfg(const GdkRGBA * gdk_rgba)
+guint rgba_to_cfg(const GdkRGBA *gdk_rgba)
 {
-    guint           cfg_int = 0;
+    guint cfg_int = 0;
 
     if (gdk_rgba == NULL)
     {
-        sat_log_log(SAT_LOG_LEVEL_ERROR,
-                    _("%s called with gdk_rgba = NULL"), __func__);
+        sat_log_log(SAT_LOG_LEVEL_ERROR, _("%s called with gdk_rgba = NULL"),
+                    __func__);
         return 0;
     }
 
-    cfg_int = ((guint) (gdk_rgba->red * 255.0)) << 24;
-    cfg_int += ((guint) (gdk_rgba->green * 255.0)) << 16;
-    cfg_int += ((guint) (gdk_rgba->blue * 255.0)) << 8;
-    cfg_int += (guint) (gdk_rgba->alpha * 255.0);
+    cfg_int = ((guint)(gdk_rgba->red * 255.0)) << 24;
+    cfg_int += ((guint)(gdk_rgba->green * 255.0)) << 16;
+    cfg_int += ((guint)(gdk_rgba->blue * 255.0)) << 8;
+    cfg_int += (guint)(gdk_rgba->alpha * 255.0);
 
     return cfg_int;
 }
 
-/* Convert a 0xRRGGBB encoded config integer to a GdkRGBA structure (no alpha channel) */
-void rgb_from_cfg(guint cfg_rgb, GdkRGBA * gdk_rgba)
+/* Convert a 0xRRGGBB encoded config integer to a GdkRGBA structure (no alpha
+ * channel) */
+void rgb_from_cfg(guint cfg_rgb, GdkRGBA *gdk_rgba)
 {
     if (gdk_rgba == NULL)
     {
-        sat_log_log(SAT_LOG_LEVEL_ERROR,
-                    _("%s called with gdk_rgba = NULL"), __func__);
+        sat_log_log(SAT_LOG_LEVEL_ERROR, _("%s called with gdk_rgba = NULL"),
+                    __func__);
         return;
     }
 
@@ -573,21 +566,22 @@ void rgb_from_cfg(guint cfg_rgb, GdkRGBA * gdk_rgba)
     gdk_rgba->alpha = 1.0;
 }
 
-/* convert GdkRGBA struct to 0xRRGGBB formatted config integer, ignoring the alpha channel */
-guint rgb_to_cfg(const GdkRGBA * gdk_rgba)
+/* convert GdkRGBA struct to 0xRRGGBB formatted config integer, ignoring the
+ * alpha channel */
+guint rgb_to_cfg(const GdkRGBA *gdk_rgba)
 {
-    guint           cfg_int = 0;
+    guint cfg_int = 0;
 
     if (gdk_rgba == NULL)
     {
-        sat_log_log(SAT_LOG_LEVEL_ERROR,
-                    _("%s called with gdk_rgba = NULL"), __func__);
+        sat_log_log(SAT_LOG_LEVEL_ERROR, _("%s called with gdk_rgba = NULL"),
+                    __func__);
         return 0;
     }
 
-    cfg_int = ((guint) (gdk_rgba->red * 255.0)) << 16;
-    cfg_int += ((guint) (gdk_rgba->green * 255.0)) << 8;
-    cfg_int += (guint) (gdk_rgba->blue * 255.0);
+    cfg_int = ((guint)(gdk_rgba->red * 255.0)) << 16;
+    cfg_int += ((guint)(gdk_rgba->green * 255.0)) << 8;
+    cfg_int += (guint)(gdk_rgba->blue * 255.0);
 
     return cfg_int;
 }
--- src/gpredict-utils.h
+++ src/gpredict-utils.h
@@ -3,48 +3,43 @@
 
 #include <gtk/gtk.h>
 
-#define M_TO_FT(x) (3.2808399*x)
-#define FT_TO_M(x) (x/3.2808399)
-#define KM_TO_MI(x) (x/1.609344)
-#define MI_TO_KM(x) (1.609344*x)
+#define M_TO_FT(x) (3.2808399 * x)
+#define FT_TO_M(x) (x / 3.2808399)
+#define KM_TO_MI(x) (x / 1.609344)
+#define MI_TO_KM(x) (1.609344 * x)
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846264338327
 #endif
 
+GtkWidget *gpredict_hpixmap_button(const gchar *file, const gchar *text,
+                                   const gchar *tooltip);
 
-GtkWidget      *gpredict_hpixmap_button(const gchar * file,
-                                        const gchar * text,
-                                        const gchar * tooltip);
+GtkWidget *gpredict_vpixmap_button(const gchar *file, const gchar *text,
+                                   const gchar *tooltip);
 
-GtkWidget      *gpredict_vpixmap_button(const gchar * file,
-                                        const gchar * text,
-                                        const gchar * tooltip);
+GtkWidget *gpredict_hstock_button(const gchar *stock_id, const gchar *text,
+                                  const gchar *tooltip);
 
-GtkWidget      *gpredict_hstock_button(const gchar * stock_id,
-                                       const gchar * text,
-                                       const gchar * tooltip);
+GtkWidget *gpredict_mini_mod_button(const gchar *pixmapfile,
+                                    const gchar *tooltip);
 
-GtkWidget      *gpredict_mini_mod_button(const gchar * pixmapfile,
-                                         const gchar * tooltip);
+void gpredict_set_combo_tooltips(GtkWidget *combo, gpointer text);
 
-void            gpredict_set_combo_tooltips(GtkWidget * combo, gpointer text);
+gint gpredict_file_copy(const gchar *in, const gchar *out);
 
-gint            gpredict_file_copy(const gchar * in, const gchar * out);
+void rgba_from_cfg(guint cfg_rgba, GdkRGBA *gdk_rgba);
+guint rgba_to_cfg(const GdkRGBA *gdk_rgba);
+void rgb_from_cfg(guint cfg_rgb, GdkRGBA *gdk_rgba);
+guint rgb_to_cfg(const GdkRGBA *gdk_rgba);
 
-void            rgba_from_cfg(guint cfg_rgba, GdkRGBA * gdk_rgba);
-guint           rgba_to_cfg(const GdkRGBA * gdk_rgba);
-void            rgb_from_cfg(guint cfg_rgb, GdkRGBA * gdk_rgba);
-guint           rgb_to_cfg(const GdkRGBA * gdk_rgba);
-
-void            rgb2gdk(guint rgb, GdkColor * color);
-void            rgba2gdk(guint rgba, GdkColor * color, guint16 * alpha);
-void            gdk2rgb(const GdkColor * color, guint * rgb);
-void            gdk2rgba(const GdkColor * color, guint16 alpha, guint * rgba);
-gchar          *rgba2html(guint rgba);
-int             gpredict_strcmp(const char *s1, const char *s2);
-char           *gpredict_strcasestr(const char *s1, const char *s2);
-gboolean        gpredict_save_key_file(GKeyFile * cfgdata,
-                                       const char *filename);
-gboolean        gpredict_legal_char(int ch);
+void rgb2gdk(guint rgb, GdkColor *color);
+void rgba2gdk(guint rgba, GdkColor *color, guint16 *alpha);
+void gdk2rgb(const GdkColor *color, guint *rgb);
+void gdk2rgba(const GdkColor *color, guint16 alpha, guint *rgba);
+gchar *rgba2html(guint rgba);
+int gpredict_strcmp(const char *s1, const char *s2);
+char *gpredict_strcasestr(const char *s1, const char *s2);
+gboolean gpredict_save_key_file(GKeyFile *cfgdata, const char *filename);
+gboolean gpredict_legal_char(int ch);
 #endif
