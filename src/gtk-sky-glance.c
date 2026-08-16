75106399c6e3c504eaff3113759d3b35a8baca20
Alexandru Csete <oz9aec@gmail.com>
2026-08-16 11:08:44 +0200

    Format GtkSkyAtGlance

--- src/gtk-sky-glance.c
+++ src/gtk-sky-glance.c
@@ -10,24 +10,24 @@
   More details can be found at the project home page:
 
   http://gpredict.oz9aec.net/
- 
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program; if not, visit http://www.fsf.org/
 */
 
 /**
  * Sky at a glance Widget.
- * 
+ *
  * The sky at a glance widget provides a convenient overview of the upcoming
  * satellite passes in a timeline format. The widget is tied to a specific
  * module and uses the QTH and satellite data from the module.
@@ -47,24 +47,24 @@
 #include "gtk-sky-glance.h"
 #include "mod-cfg-get-param.h"
 #include "predict-tools.h"
-#include "sat-pass-dialogs.h"
 #include "sat-cfg.h"
 #include "sat-log.h"
+#include "sat-pass-dialogs.h"
 #include "sgpsdp/sgp4sdp4.h"
 #include "time-tools.h"
 
-
-#define SKG_DEFAULT_WIDTH       600
-#define SKG_DEFAULT_HEIGHT      300
-#define SKG_PIX_PER_SAT         10
-#define SKG_MARGIN              20
-#define SKG_FOOTER              50
-#define SKG_CURSOR_WIDTH        0.5
+#define SKG_DEFAULT_WIDTH 600
+#define SKG_DEFAULT_HEIGHT 300
+#define SKG_PIX_PER_SAT 10
+#define SKG_MARGIN 20
+#define SKG_FOOTER 50
+#define SKG_CURSOR_WIDTH 0.5
 
 static GtkBoxClass *parent_class = NULL;
 
 /** Convert rgba color to cairo-friendly format */
-static void rgba_to_cairo(guint32 rgba, gdouble *r, gdouble *g, gdouble *b, gdouble *a)
+static void rgba_to_cairo(guint32 rgba, gdouble *r, gdouble *g, gdouble *b,
+                          gdouble *a)
 {
     *r = ((rgba >> 24) & 0xFF) / 255.0;
     *g = ((rgba >> 16) & 0xFF) / 255.0;
@@ -72,8 +72,7 @@ static void rgba_to_cairo(guint32 rgba, gdouble *r, gdouble *g, gdouble *b, gdou
     *a = (rgba & 0xFF) / 255.0;
 }
 
-static void gtk_sky_glance_init(GtkSkyGlance * skg,
-	gpointer g_class)
+static void gtk_sky_glance_init(GtkSkyGlance *skg, gpointer g_class)
 {
     (void)g_class;
 
@@ -109,11 +108,11 @@ static void free_sat_label(gpointer data)
     }
 }
 
-static void gtk_sky_glance_destroy(GtkWidget * widget)
+static void gtk_sky_glance_destroy(GtkWidget *widget)
 {
     GtkSkyGlance *skg = GTK_SKY_GLANCE(widget);
-    sky_pass_t   *skypass;
-    guint         i, n;
+    sky_pass_t *skypass;
+    guint i, n;
 
     /* free passes */
     /* FIXME: TBC whether this is enough */
@@ -160,44 +159,41 @@ static void gtk_sky_glance_destroy(GtkWidget * widget)
     g_free(skg->font);
     skg->font = NULL;
 
-    (*GTK_WIDGET_CLASS(parent_class)->destroy) (widget);
+    (*GTK_WIDGET_CLASS(parent_class)->destroy)(widget);
 }
 
-
-static void gtk_sky_glance_class_init(GtkSkyGlanceClass * class,
-				      gpointer class_data)
+static void gtk_sky_glance_class_init(GtkSkyGlanceClass *class,
+                                      gpointer class_data)
 {
     GtkWidgetClass *widget_class;
 
     (void)class_data;
 
-    widget_class = (GtkWidgetClass *) class;
+    widget_class = (GtkWidgetClass *)class;
     widget_class->destroy = gtk_sky_glance_destroy;
     parent_class = g_type_class_peek_parent(class);
 }
 
 GType gtk_sky_glance_get_type()
 {
-    static GType    gtk_sky_glance_type = 0;
+    static GType gtk_sky_glance_type = 0;
 
     if (!gtk_sky_glance_type)
     {
         static const GTypeInfo gtk_sky_glance_info = {
             sizeof(GtkSkyGlanceClass),
-            NULL,               /* base init */
-            NULL,               /* base finalise */
-            (GClassInitFunc) gtk_sky_glance_class_init,
-            NULL,               /* class finalise */
-            NULL,               /* class data */
+            NULL, /* base init */
+            NULL, /* base finalise */
+            (GClassInitFunc)gtk_sky_glance_class_init,
+            NULL, /* class finalise */
+            NULL, /* class data */
             sizeof(GtkSkyGlance),
-            1,                  /* n_preallocs */
-            (GInstanceInitFunc) gtk_sky_glance_init,
-            NULL
-        };
+            1, /* n_preallocs */
+            (GInstanceInitFunc)gtk_sky_glance_init,
+            NULL};
 
-        gtk_sky_glance_type = g_type_register_static(GTK_TYPE_BOX,
-                                                     "GtkSkyGlance",
-                                                     &gtk_sky_glance_info, 0);
+        gtk_sky_glance_type = g_type_register_static(
+            GTK_TYPE_BOX, "GtkSkyGlance", &gtk_sky_glance_info, 0);
     }
 
     return gtk_sky_glance_type;
@@ -206,9 +202,9 @@ GType gtk_sky_glance_get_type()
 /**
  * Convert time value to x position.
  */
-static gdouble t2x(GtkSkyGlance * skg, gdouble t)
+static gdouble t2x(GtkSkyGlance *skg, gdouble t)
 {
-    gdouble         frac;
+    gdouble frac;
 
     frac = (t - skg->ts) / (skg->te - skg->ts);
 
@@ -218,9 +214,9 @@ static gdouble t2x(GtkSkyGlance * skg, gdouble t)
 /**
  * Convert x coordinate to Julian date.
  */
-static gdouble x2t(GtkSkyGlance * skg, gdouble x)
+static gdouble x2t(GtkSkyGlance *skg, gdouble x)
 {
-    gdouble         frac;
+    gdouble frac;
 
     frac = (x - skg->x0) / skg->w;
 
@@ -229,7 +225,7 @@ static gdouble x2t(GtkSkyGlance * skg, gdouble x)
 
 static sky_pass_t *find_pass_at_pos(GtkSkyGlance *skg, gdouble mx, gdouble my)
 {
-    GSList     *node;
+    GSList *node;
     sky_pass_t *skypass;
 
     node = skg->passes;
@@ -248,21 +244,22 @@ static sky_pass_t *find_pass_at_pos(GtkSkyGlance *skg, gdouble mx, gdouble my)
 
 static gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
 {
-    GtkSkyGlance   *skg = GTK_SKY_GLANCE(data);
-    gdouble         r, g, b, a;
-    PangoLayout    *layout;
+    GtkSkyGlance *skg = GTK_SKY_GLANCE(data);
+    gdouble r, g, b, a;
+    PangoLayout *layout;
     PangoFontDescription *font_desc;
-    gint            tw, th;
-    guint           i;
-    sky_pass_t     *skypass;
-    GSList         *node;
-    sat_label_t    *label;
+    gint tw, th;
+    guint i;
+    sky_pass_t *skypass;
+    GSList *node;
+    sat_label_t *label;
 
     (void)widget;
 
     /* Set up font */
     layout = pango_cairo_create_layout(cr);
-    font_desc = pango_font_description_from_string(skg->font ? skg->font : "Sans 9");
+    font_desc =
+        pango_font_description_from_string(skg->font ? skg->font : "Sans 9");
     pango_layout_set_font_description(layout, font_desc);
 
     /* Background */
@@ -303,9 +300,9 @@ static gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
         pango_layout_set_text(layout, label->name, -1);
         pango_layout_get_pixel_size(layout, &tw, &th);
 
-        if (label->anchor == 1)  /* East anchor */
+        if (label->anchor == 1) /* East anchor */
             cairo_move_to(cr, label->x - tw, label->y - th / 2);
-        else  /* West anchor */
+        else /* West anchor */
             cairo_move_to(cr, label->x, label->y - th / 2);
 
         pango_cairo_show_layout(cr, layout);
@@ -377,7 +374,9 @@ static gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
 
     /* Axis label */
     {
-        const gchar *axis_text = sat_cfg_get_bool(SAT_CFG_BOOL_USE_LOCAL_TIME) ? _("TIME") : _("UTC");
+        const gchar *axis_text = sat_cfg_get_bool(SAT_CFG_BOOL_USE_LOCAL_TIME)
+                                     ? _("TIME")
+                                     : _("UTC");
         pango_layout_set_text(layout, axis_text, -1);
         pango_layout_get_pixel_size(layout, &tw, &th);
         cairo_move_to(cr, skg->w / 2 - tw / 2, skg->h + SKG_FOOTER - 5 - th);
@@ -390,11 +389,12 @@ static gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
     return FALSE;
 }
 
-static gboolean on_motion_notify(GtkWidget *widget, GdkEventMotion *event, gpointer data)
+static gboolean on_motion_notify(GtkWidget *widget, GdkEventMotion *event,
+                                 gpointer data)
 {
-    GtkSkyGlance   *skg = GTK_SKY_GLANCE(data);
-    gdouble         t;
-    gchar           buff[6];
+    GtkSkyGlance *skg = GTK_SKY_GLANCE(data);
+    gdouble t;
+    gchar buff[6];
 
     (void)widget;
 
@@ -412,11 +412,12 @@ static gboolean on_motion_notify(GtkWidget *widget, GdkEventMotion *event, gpoin
     return TRUE;
 }
 
-static gboolean on_button_release(GtkWidget *widget, GdkEventButton *event, gpointer data)
+static gboolean on_button_release(GtkWidget *widget, GdkEventButton *event,
+                                  gpointer data)
 {
-    GtkSkyGlance   *skg = GTK_SKY_GLANCE(data);
-    sky_pass_t     *skypass;
-    pass_t         *new_pass;
+    GtkSkyGlance *skg = GTK_SKY_GLANCE(data);
+    sky_pass_t *skypass;
+    pass_t *new_pass;
 
     (void)widget;
 
@@ -430,14 +431,13 @@ static gboolean on_button_release(GtkWidget *widget, GdkEventButton *event, gpoi
     if (skypass->pass == NULL)
     {
         sat_log_log(SAT_LOG_LEVEL_ERROR,
-                    _("%s::%s: Could not retrieve pass_t object"),
-                    __FILE__, __func__);
+                    _("%s::%s: Could not retrieve pass_t object"), __FILE__,
+                    __func__);
         return TRUE;
     }
 
     new_pass = copy_pass(skypass->pass);
-    sat_log_log(SAT_LOG_LEVEL_DEBUG,
-                _("%s::%s: Showing pass details for %s"),
+    sat_log_log(SAT_LOG_LEVEL_DEBUG, _("%s::%s: Showing pass details for %s"),
                 __FILE__, __func__, skypass->pass->satname);
 
     /* show the pass details */
@@ -446,17 +446,17 @@ static gboolean on_button_release(GtkWidget *widget, GdkEventButton *event, gpoi
     return TRUE;
 }
 
-static void size_allocate_cb(GtkWidget * widget, GtkAllocation * allocation,
+static void size_allocate_cb(GtkWidget *widget, GtkAllocation *allocation,
                              gpointer data)
 {
-    GtkSkyGlance   *skg;
-    gint            i, j, n;
-    guint           curcat;
-    gdouble         th, tm;
-    sky_pass_t     *skp;
-    gdouble         x, y, w, h;
-    sat_label_t    *label;
-    GSList         *node;
+    GtkSkyGlance *skg;
+    gint i, j, n;
+    guint curcat;
+    gdouble th, tm;
+    sky_pass_t *skp;
+    gdouble x, y, w, h;
+    sat_label_t *label;
+    GSList *node;
 
     if (gtk_widget_get_realized(widget))
     {
@@ -469,7 +469,7 @@ static void size_allocate_cb(GtkWidget * widget, GtkAllocation * allocation,
 
         /* Update tick positions */
         th = ceil(skg->ts * 24.0) / 24.0;
-        th += 0.00069;  /* workaround for bug 1839140 */
+        th += 0.00069; /* workaround for bug 1839140 */
 
         if ((th - skg->ts) > 0.0208333)
             tm = th - 0.0208333;
@@ -517,12 +517,12 @@ static void size_allocate_cb(GtkWidget * widget, GtkAllocation * allocation,
                     if (x > (skg->x0 + 100))
                     {
                         label->x = x - 5;
-                        label->anchor = 1;  /* East */
+                        label->anchor = 1; /* East */
                     }
                     else
                     {
                         label->x = x + w + 5;
-                        label->anchor = 0;  /* West */
+                        label->anchor = 0; /* West */
                     }
                     node = node->next;
                 }
@@ -538,18 +538,18 @@ static void size_allocate_cb(GtkWidget * widget, GtkAllocation * allocation,
     }
 }
 
-static void on_canvas_realized(GtkWidget * canvas, gpointer data)
+static void on_canvas_realized(GtkWidget *canvas, gpointer data)
 {
-    GtkAllocation   aloc;
+    GtkAllocation aloc;
 
     gtk_widget_get_allocation(canvas, &aloc);
     size_allocate_cb(canvas, &aloc, data);
 }
 
 /** Fetch the basic colour and add alpha channel */
-static void get_colors(guint i, guint * bcol, guint * fcol)
+static void get_colors(guint i, guint *bcol, guint *fcol)
 {
-    guint           tmp;
+    guint tmp;
 
     /* ensure that we are within 1..10 */
     i = i % 10;
@@ -598,8 +598,8 @@ static void get_colors(guint i, guint * bcol, guint * fcol)
 
     default:
         sat_log_log(SAT_LOG_LEVEL_ERROR,
-                    _("%s:%d: Colour index out of valid range (%d)"),
-                    __FILE__, __LINE__, i);
+                    _("%s:%d: Colour index out of valid range (%d)"), __FILE__,
+                    __LINE__, i);
         tmp = sat_cfg_get_int(SAT_CFG_INT_SKYATGL_COL_01);
         break;
     }
@@ -616,15 +616,15 @@ static void get_colors(guint i, guint * bcol, guint * fcol)
  */
 static void create_sat(gpointer key, gpointer value, gpointer data)
 {
-    sat_t          *sat = SAT(value);
-    GtkSkyGlance   *skg = GTK_SKY_GLANCE(data);
-    GSList         *passes = NULL;
-    gdouble         maxdt;
-    guint           i, n;
-    pass_t         *tmppass = NULL;
-    sky_pass_t     *skypass;
-    guint           bcol, fcol;
-    sat_label_t    *label;
+    sat_t *sat = SAT(value);
+    GtkSkyGlance *skg = GTK_SKY_GLANCE(data);
+    GSList *passes = NULL;
+    gdouble maxdt;
+    guint i, n;
+    pass_t *tmppass = NULL;
+    sky_pass_t *skypass;
+    guint bcol, fcol;
+    sat_label_t *label;
 
     (void)key;
 
@@ -635,8 +635,8 @@ static void create_sat(gpointer key, gpointer value, gpointer data)
     passes = get_passes(sat, skg->qth, skg->ts, maxdt, 10);
     n = g_slist_length(passes);
     sat_log_log(SAT_LOG_LEVEL_DEBUG,
-                _("%s:%d: %s has %d passes within %.4f days\n"),
-                __FILE__, __LINE__, sat->nickname, n, maxdt);
+                _("%s:%d: %s has %d passes within %.4f days\n"), __FILE__,
+                __LINE__, sat->nickname, n, maxdt);
 
     if (passes != NULL)
     {
@@ -647,8 +647,8 @@ static void create_sat(gpointer key, gpointer value, gpointer data)
             if (skypass == NULL)
             {
                 sat_log_log(SAT_LOG_LEVEL_ERROR,
-                            _("%s:%s: Could not allocate memory."),
-                            __FILE__, __func__);
+                            _("%s:%s: Could not allocate memory."), __FILE__,
+                            __func__);
                 continue;
             }
 
@@ -686,11 +686,11 @@ static void create_sat(gpointer key, gpointer value, gpointer data)
 /**
  * Create the time tick data
  */
-static void create_time_ticks(GtkSkyGlance * skg)
+static void create_time_ticks(GtkSkyGlance *skg)
 {
-    guint           i;
-    gdouble         th, tm;
-    gchar           buff[3];
+    guint i;
+    gdouble th, tm;
+    gchar buff[3];
 
     skg->num_ticks = sat_cfg_get_int(SAT_CFG_INT_SKYATGL_TIME);
 
@@ -700,7 +700,7 @@ static void create_time_ticks(GtkSkyGlance * skg)
 
     /* get the first hour and first 30 min slot */
     th = ceil(skg->ts * 24.0) / 24.0;
-    th += 0.00069;  /* workaround for bug 1839140 */
+    th += 0.00069; /* workaround for bug 1839140 */
 
     if ((th - skg->ts) > 0.0208333)
         tm = th - 0.0208333;
@@ -727,11 +727,11 @@ static void create_time_ticks(GtkSkyGlance * skg)
  * @param qth Pointer to the ground station data.
  * @param ts The t0 for the timeline or 0 to use the current date and time.
  */
-GtkWidget      *gtk_sky_glance_new(GHashTable * sats, qth_t * qth, gdouble ts)
+GtkWidget *gtk_sky_glance_new(GHashTable *sats, qth_t *qth, gdouble ts)
 {
-    GtkSkyGlance   *skg;
-    guint           number;
-    GValue          font_value = G_VALUE_INIT;
+    GtkSkyGlance *skg;
+    guint number;
+    GValue font_value = G_VALUE_INIT;
 
     /* check that we have at least one satellite */
     number = g_hash_table_size(sats);
@@ -743,7 +743,8 @@ GtkWidget      *gtk_sky_glance_new(GHashTable * sats, qth_t * qth, gdouble ts)
 
     /* default font */
     g_value_init(&font_value, G_TYPE_STRING);
-    g_object_get_property(G_OBJECT(gtk_settings_get_default()), "gtk-font-name", &font_value);
+    g_object_get_property(G_OBJECT(gtk_settings_get_default()), "gtk-font-name",
+                          &font_value);
     skg->font = g_value_dup_string(&font_value);
     g_value_unset(&font_value);
 
@@ -756,7 +757,8 @@ GtkWidget      *gtk_sky_glance_new(GHashTable * sats, qth_t * qth, gdouble ts)
 
     /* if ts = 0 use current time */
     skg->ts = ts > 0.0 ? ts : get_current_daynum();
-    skg->te = skg->ts + sat_cfg_get_int(SAT_CFG_INT_SKYATGL_TIME) * (1.0 / 24.0);
+    skg->te =
+        skg->ts + sat_cfg_get_int(SAT_CFG_INT_SKYATGL_TIME) * (1.0 / 24.0);
 
     /* calculate preferred sizes */
     skg->w = SKG_DEFAULT_WIDTH;
@@ -768,14 +770,19 @@ GtkWidget      *gtk_sky_glance_new(GHashTable * sats, qth_t * qth, gdouble ts)
     gtk_widget_set_has_tooltip(skg->canvas, TRUE);
     gtk_widget_set_size_request(skg->canvas, skg->w, skg->h + SKG_FOOTER);
     gtk_widget_add_events(skg->canvas, GDK_POINTER_MOTION_MASK |
-                          GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK);
+                                           GDK_BUTTON_PRESS_MASK |
+                                           GDK_BUTTON_RELEASE_MASK);
 
     /* connect signals */
     g_signal_connect(skg->canvas, "draw", G_CALLBACK(on_draw), skg);
-    g_signal_connect(skg->canvas, "motion-notify-event", G_CALLBACK(on_motion_notify), skg);
-    g_signal_connect(skg->canvas, "button-release-event", G_CALLBACK(on_button_release), skg);
-    g_signal_connect(skg->canvas, "size-allocate", G_CALLBACK(size_allocate_cb), skg);
-    g_signal_connect_after(skg->canvas, "realize", G_CALLBACK(on_canvas_realized), skg);
+    g_signal_connect(skg->canvas, "motion-notify-event",
+                     G_CALLBACK(on_motion_notify), skg);
+    g_signal_connect(skg->canvas, "button-release-event",
+                     G_CALLBACK(on_button_release), skg);
+    g_signal_connect(skg->canvas, "size-allocate", G_CALLBACK(size_allocate_cb),
+                     skg);
+    g_signal_connect_after(skg->canvas, "realize",
+                           G_CALLBACK(on_canvas_realized), skg);
 
     gtk_widget_show(skg->canvas);
 
--- src/gtk-sky-glance.h
+++ src/gtk-sky-glance.h
@@ -10,123 +10,123 @@
     More details can be found at the project home page:
 
             http://gpredict.oz9aec.net/
- 
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
-  
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-  
+
     You should have received a copy of the GNU General Public License
     along with this program; if not, visit http://www.fsf.org/
 */
 #ifndef __GTK_SKY_GLANCE_H__
 #define __GTK_SKY_GLANCE_H__ 1
 
+#include "gtk-sat-data.h"
 #include <gdk/gdk.h>
 #include <glib.h>
 #include <glib/gi18n.h>
 #include <gtk/gtk.h>
-#include "gtk-sat-data.h"
 
 #include "predict-tools.h"
 
-/* *INDENT-OFF* */
 #ifdef __cplusplus
-extern          "C" {
+extern "C" {
 #endif
-/* *INDENT-ON* */
 
-
+/* clang-format off */
 #define GTK_SKY_GLANCE(obj)          G_TYPE_CHECK_INSTANCE_CAST (obj, gtk_sky_glance_get_type (), GtkSkyGlance)
 #define GTK_SKY_GLANCE_CLASS(klass)  G_TYPE_CHECK_CLASS_CAST (klass, gtk_sky_glance_get_type (), GtkSkyGlanceClass)
 #define GTK_IS_SKY_GLANCE(obj)       G_TYPE_CHECK_INSTANCE_TYPE (obj, gtk_sky_glance_get_type ())
 #define GTK_TYPE_SKY_GLANCE          (gtk_sky_glance_get_type ())
 #define IS_GTK_SKY_GLANCE(obj)       G_TYPE_CHECK_INSTANCE_TYPE (obj, gtk_sky_glance_get_type ())
+/* clang-format on */
 
 typedef struct _GtkSkyGlance GtkSkyGlance;
 typedef struct _GtkSkyGlanceClass GtkSkyGlanceClass;
 
-
-/** Satellite object on graph. */
+/* Satellite object on graph. */
 typedef struct {
-    guint           catnum;     /* Catalog number of satellite */
-    pass_t         *pass;       /* Details of the corresponding pass. */
-    gdouble         x;          /* X position of box */
-    gdouble         y;          /* Y position of box */
-    gdouble         w;          /* Width of box */
-    gdouble         h;          /* Height of box */
-    guint32         bcol;       /* Border color */
-    guint32         fcol;       /* Fill color */
+    guint catnum; /* Catalog number of satellite */
+    pass_t *pass; /* Details of the corresponding pass. */
+    gdouble x;    /* X position of box */
+    gdouble y;    /* Y position of box */
+    gdouble w;    /* Width of box */
+    gdouble h;    /* Height of box */
+    guint32 bcol; /* Border color */
+    guint32 fcol; /* Fill color */
 } sky_pass_t;
-
 
 #define SKY_PASS_T(obj) ((sky_pass_t *)obj)
 
-
-/** GtkSkyGlance widget */
+/* GtkSkyGlance widget */
 struct _GtkSkyGlance {
-    GtkBox          vbox;
+    GtkBox vbox;
 
-    GtkWidget      *canvas;     /*!< The drawing area widget */
+    GtkWidget *canvas; /* The drawing area widget */
 
-    GHashTable     *sats;       /* Local copy of satellites. */
-    qth_t          *qth;        /* Pointer to current location. */
+    GHashTable *sats; /* Local copy of satellites. */
+    qth_t *qth;       /* Pointer to current location. */
 
-    GSList         *passes;     /* List of sky_pass_t representing each pass. */
-    GSList         *satlab;     /* List of satellite label data (name, color, position). */
+    GSList *passes; /* List of sky_pass_t representing each pass. */
+    GSList *satlab; /* List of satellite label data (name, color, position).
+                     * Satellite label data structure
+                     * Each element is a struct
+                     * {
+                     *     gchar *name;
+                     *     guint32 color;
+                     *     gdouble x, y;
+                     *  }
+                     */
 
-    /* Satellite label data structure */
-    /* Each element is a struct { gchar *name; guint32 color; gdouble x, y; } */
+    guint x0;
+    guint y0;
+    guint w;   /* width of the plot */
+    guint h;   /* height of the plot */
+    guint pps; /* pixels per satellite */
 
-    guint           x0;
-    guint           y0;
-    guint           w;          /* width of the plot */
-    guint           h;          /* height of the plot */
-    guint           pps;        /* pixels per satellite */
-
-    guint           numsat;     /* Number of satellites */
-    guint           satcnt;     /* Counter to keep track of how many satellites we have
-                                   plotted so far when creating the boxes.
-                                   This is needed to ensure that we do not plot more
-                                   than 10 satellites and to know which colour to fetch
-                                   from sat-cfg.
-                                 */
-    gdouble         ts, te;     /* Start and end times (Julian date) */
+    guint numsat;   /* Number of satellites */
+    guint satcnt;   /* Counter to keep track of how many satellites we have
+                       plotted so far when creating the boxes.
+                       This is needed to ensure that we do not plot more
+                       than 10 satellites and to know which colour to fetch
+                       from sat-cfg.
+                     */
+    gdouble ts, te; /* Start and end times (Julian date) */
 
     /* Time tick data */
-    gint            num_ticks;  /* Number of time ticks */
-    gdouble        *major_x;    /* X positions of major ticks */
-    gdouble        *minor_x;    /* X positions of minor ticks */
-    gchar         **tick_labels; /* Tick label strings */
+    gint num_ticks;      /* Number of time ticks */
+    gdouble *major_x;    /* X positions of major ticks */
+    gdouble *minor_x;    /* X positions of minor ticks */
+    gchar **tick_labels; /* Tick label strings */
 
     /* Cursor tracking */
-    gdouble         cursor_x;   /* Current cursor X position */
-    gchar          *time_label; /* Time label text at cursor */
+    gdouble cursor_x;  /* Current cursor X position */
+    gchar *time_label; /* Time label text at cursor */
 
-    gchar          *font;       /*!< Default font name */
-
+    gchar *font; /*!< Default font name */
 };
 
-/** Satellite label structure for drawing */
+/* Satellite label structure for drawing */
 typedef struct {
-    gchar          *name;       /* Satellite name */
-    guint32         color;      /* Label color */
-    gdouble         x;          /* X position */
-    gdouble         y;          /* Y position */
-    gint            anchor;     /* Anchor type: 0=west, 1=east */
+    gchar *name;   /* Satellite name */
+    guint32 color; /* Label color */
+    gdouble x;     /* X position */
+    gdouble y;     /* Y position */
+    gint anchor;   /* Anchor type: 0=west, 1=east */
 } sat_label_t;
+
 struct _GtkSkyGlanceClass {
-    GtkBoxClass     parent_class;
+    GtkBoxClass parent_class;
 };
 
-
-GType           gtk_sky_glance_get_type(void);
-GtkWidget      *gtk_sky_glance_new(GHashTable * sats, qth_t * qth, gdouble ts);
+GType gtk_sky_glance_get_type(void);
+GtkWidget *gtk_sky_glance_new(GHashTable *sats, qth_t *qth, gdouble ts);
 
 /* *INDENT-OFF* */
 #ifdef __cplusplus
